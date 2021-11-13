561c238f6d2042e450df9ff7139d7a8e733b6875
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-13 15:39:32 +0100

    Add unit test for scc_vec_reserve

--- test/unit/scc_vec_test.c
+++ test/unit/scc_vec_test.c
@@ -21,3 +21,17 @@ void test_scc_vec_capacity(void) {
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_vec_capacity(vec));
     scc_vec_free(vec);
 }
+
+void test_scc_vec_reserve(void) {
+    int *vec = scc_vec_init();
+    TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_vec_capacity(vec));
+    TEST_ASSERT_TRUE(scc_vec_reserve(vec, 13));
+    TEST_ASSERT_EQUAL_UINT64(13u, (unsigned long long)scc_vec_capacity(vec));
+    TEST_ASSERT_TRUE(scc_vec_reserve(vec, 3));
+    TEST_ASSERT_EQUAL_UINT64(13u, (unsigned long long)scc_vec_capacity(vec));
+    TEST_ASSERT_TRUE(scc_vec_reserve(vec, 32));
+    TEST_ASSERT_EQUAL_UINT64(32u, (unsigned long long)scc_vec_capacity(vec));
+    TEST_ASSERT_TRUE(scc_vec_reserve(vec, 87));
+    TEST_ASSERT_EQUAL_UINT64(87u, (unsigned long long)scc_vec_capacity(vec));
+    scc_vec_free(vec);
+}
