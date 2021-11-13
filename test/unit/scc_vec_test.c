9707af2f72a372de0dd6af47ec4e3b75fe66f6b0
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-13 15:28:29 +0100

    Add scc_vec_free calls for all tests

--- test/unit/scc_vec_test.c
+++ test/unit/scc_vec_test.c
@@ -7,14 +7,17 @@ void test_scc_vec_init(void) {
     int *vec = scc_vec_init();
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_container(vec, struct scc_vec, sc_buffer)->sc_size);
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_container(vec, struct scc_vec, sc_buffer)->sc_capacity);
+    scc_vec_free(vec);
 }
 
 void test_scc_vec_size(void) {
     int *vec = scc_vec_init();
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_vec_size(vec));
+    scc_vec_free(vec);
 }
 
 void test_scc_vec_capacity(void) {
     int *vec = scc_vec_init();
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_vec_capacity(vec));
+    scc_vec_free(vec);
 }
