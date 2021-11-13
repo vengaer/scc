ccf43da616f1cc715c8691aeb7d34d978d63a7db
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-13 15:09:50 +0100

    Add unit test for scc_vec_size

--- test/unit/scc_vec_test.c
+++ test/unit/scc_vec_test.c
@@ -3,8 +3,13 @@
 
 #include <unity.h>
 
-void test_vec_init(void) {
+void test_scc_vec_init(void) {
     int *vec = scc_vec_init();
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_container(vec, struct scc_vec, sc_buffer)->sc_size);
     TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_container(vec, struct scc_vec, sc_buffer)->sc_capacity);
+}
+
+void test_scc_vec_size(void) {
+    int *vec = scc_vec_init();
+    TEST_ASSERT_EQUAL_UINT64(0u, (unsigned long long)scc_vec_size(vec));
 }
