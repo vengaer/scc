24739a33212e4bf9e1c1aef3a11456ae45a5aaf2
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-13 23:55:48 +0100

    Add unit tests verifying behavior when passing invalid ranges to erase_range

--- test/unit/scc_vec_test.c
+++ test/unit/scc_vec_test.c
@@ -311,3 +311,33 @@ void test_scc_vec_erase_range(void) {
 
     scc_vec_free(vec);
 }
+
+void test_scc_vec_erase_range_end_lt_first(void) {
+    int *vec = scc_vec_init();
+
+    for(int i = 0; i < 100; i++) {
+        TEST_ASSERT_TRUE(scc_vec_push(vec, i));
+    }
+
+    scc_vec_erase_range(vec, &vec[3], &vec[1]);
+    TEST_ASSERT_EQUAL_UINT64(100, scc_vec_size(vec));
+    for(int i = 0; i < 100; i++) {
+        TEST_ASSERT_EQUAL_INT32(i, vec[i]);
+    }
+    scc_vec_free(vec);
+}
+
+void test_scc_vec_erase_range_end_eq_first(void) {
+    int *vec = scc_vec_init();
+
+    for(int i = 0; i < 100; i++) {
+        TEST_ASSERT_TRUE(scc_vec_push(vec, i));
+    }
+
+    scc_vec_erase_range(vec, &vec[3], &vec[3]);
+    TEST_ASSERT_EQUAL_UINT64(100, scc_vec_size(vec));
+    for(int i = 0; i < 100; i++) {
+        TEST_ASSERT_EQUAL_INT32(i, vec[i]);
+    }
+    scc_vec_free(vec);
+}
