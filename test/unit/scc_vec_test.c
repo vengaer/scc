4fa87f19c5325638261c8bfabfeca3c5b4b2cd4e
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-13 18:37:33 +0100

    Add unit test for scc_vec_push

--- test/unit/scc_vec_test.c
+++ test/unit/scc_vec_test.c
@@ -35,3 +35,40 @@ void test_scc_vec_reserve(void) {
     TEST_ASSERT_EQUAL_UINT64(87u, (unsigned long long)scc_vec_capacity(vec));
     scc_vec_free(vec);
 }
+
+void test_scc_vec_push(void) {
+    int *vec = scc_vec_init();
+    TEST_ASSERT_TRUE(scc_vec_push(vec, 1));
+    TEST_ASSERT_EQUAL_UINT64(1u, (unsigned long long)scc_vec_size(vec));
+    TEST_ASSERT_EQUAL_INT32(1u, vec[0]);
+    TEST_ASSERT_TRUE(scc_vec_push(vec, 2));
+    TEST_ASSERT_EQUAL_UINT64(2u, (unsigned long long)scc_vec_size(vec));
+    TEST_ASSERT_EQUAL_INT32(2u, vec[1]);
+    scc_vec_free(vec);
+}
+
+void test_scc_vec_push_allocation_pattern(void) {
+    int *vec = scc_vec_init();
+    size_t ccap = 0u;
+
+    while(ccap < 4096u) {
+        while(scc_vec_size(vec) < ccap) {
+            TEST_ASSERT_TRUE(scc_vec_push(vec, 1));
+            TEST_ASSERT_EQUAL_UINT64(ccap, (unsigned long long)scc_vec_capacity(vec));
+        }
+
+        ccap = 2 * ccap + !(ccap & 1);
+    }
+
+    while(ccap < 16384u) {
+        while(scc_vec_size(vec) < ccap) {
+            TEST_ASSERT_TRUE(scc_vec_push(vec, 1));
+            TEST_ASSERT_EQUAL_UINT64(ccap, (unsigned long long)scc_vec_capacity(vec));
+        }
+
+        ccap += 4096u;
+    }
+
+
+    scc_vec_free(vec);
+}
