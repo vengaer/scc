15a2f83d4f781b89c7f373dafb4bdae6f8ac3111
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-20 13:56:19 +0100

    Add unit test for reserve

--- test/unit/scc_svec_test.c
+++ test/unit/scc_svec_test.c
@@ -33,3 +33,17 @@ void test_scc_svec_capacity(void) {
     TEST_ASSERT_EQUAL_UINT64(96ull, scc_svec_capacity(svec));
     scc_svec_free(svec);
 }
+
+void test_scc_svec_reserve(void) {
+    int *svec = scc_svec_init(int);
+    TEST_ASSERT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY, scc_svec_capacity(svec));
+    TEST_ASSERT_TRUE(scc_svec_reserve(svec, 10));
+    TEST_ASSERT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY, scc_svec_capacity(svec));
+    TEST_ASSERT_TRUE(scc_svec_reserve(svec, 128));
+    TEST_ASSERT_EQUAL_UINT64(128ull, scc_svec_capacity(svec));
+    TEST_ASSERT_TRUE(scc_svec_reserve(svec, 18));
+    TEST_ASSERT_EQUAL_UINT64(128ull, scc_svec_capacity(svec));
+    TEST_ASSERT_TRUE(scc_svec_reserve(svec, 1024));
+    TEST_ASSERT_EQUAL_UINT64(1024ull, scc_svec_capacity(svec));
+    scc_svec_free(svec);
+}
