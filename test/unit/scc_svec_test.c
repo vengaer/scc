0a55227dc61eb0b9cf0c98baf3ae0e81cd24470e
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-20 13:47:31 +0100

    Add unit test for scc_svec_capacity

--- test/unit/scc_svec_test.c
+++ test/unit/scc_svec_test.c
@@ -17,6 +17,19 @@ void test_scc_svec_size(void) {
     TEST_ASSERT_EQUAL_UINT64(3ull, scc_svec_size(svec));
     scc_svec_impl_base(svec)->sc_size = 18ull;
     TEST_ASSERT_EQUAL_UINT64(18ull, scc_svec_size(svec));
-    scc_svec_impl_base(svec)->sc_size = 0ull;
+    scc_svec_free(svec);
+}
+
+void test_scc_svec_capacity(void) {
+    struct foo {
+        unsigned long long ull;
+        char c;
+    };
+    struct foo *svec = scc_svec_init(struct foo);
+    TEST_ASSERT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY, scc_svec_capacity(svec));
+    scc_svec_impl_base(svec)->sc_capacity = 38ull;
+    TEST_ASSERT_EQUAL_UINT64(38ull, scc_svec_capacity(svec));
+    scc_svec_impl_base(svec)->sc_capacity = 96ull;
+    TEST_ASSERT_EQUAL_UINT64(96ull, scc_svec_capacity(svec));
     scc_svec_free(svec);
 }
