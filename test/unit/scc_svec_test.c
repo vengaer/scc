8a69ca44ba1300a9809a9790e3f3d19d3f15a41f
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-20 15:58:04 +0100

    Add test for scc_svec_pop

--- test/unit/scc_svec_test.c
+++ test/unit/scc_svec_test.c
@@ -180,3 +180,15 @@ void test_scc_svec_resize_sizeup_nonempty(void) {
 
     scc_svec_free(svec);
 }
+
+void test_scc_svec_pop(void) {
+    int *svec = scc_svec_from(int, 1, 2, 3);
+    TEST_ASSERT_EQUAL_UINT64(3ull, scc_svec_size(svec));
+    scc_svec_pop(svec);
+    TEST_ASSERT_EQUAL_UINT64(2ull, scc_svec_size(svec));
+    scc_svec_pop(svec);
+    TEST_ASSERT_EQUAL_UINT64(1ull, scc_svec_size(svec));
+    scc_svec_pop(svec);
+    TEST_ASSERT_EQUAL_UINT64(0ull, scc_svec_size(svec));
+    scc_svec_free(svec);
+}
