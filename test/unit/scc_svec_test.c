b6746f366a0a99a1778f89234c3c4f962daf2ad4
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-20 13:44:54 +0100

    Add unit test for scc_svec_size

--- test/unit/scc_svec_test.c
+++ test/unit/scc_svec_test.c
@@ -2,10 +2,21 @@
 
 #include <unity.h>
 
-void test_svec_init(void) {
+void test_scc_svec_init(void) {
     int *svec = scc_svec_init(int);
     TEST_ASSERT_EQUAL_UINT64(0ull, scc_svec_impl_base(svec)->sc_size);
     TEST_ASSERT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY, scc_svec_impl_base(svec)->sc_capacity);
     TEST_ASSERT_EQUAL_UINT8(((unsigned char *)svec)[-1], 0);
+    scc_svec_free(svec);
+}
+
+void test_scc_svec_size(void) {
+    int *svec = scc_svec_init(int);
+    TEST_ASSERT_EQUAL_UINT64(0ull, scc_svec_size(svec));
+    scc_svec_impl_base(svec)->sc_size = 3ull;
+    TEST_ASSERT_EQUAL_UINT64(3ull, scc_svec_size(svec));
+    scc_svec_impl_base(svec)->sc_size = 18ull;
+    TEST_ASSERT_EQUAL_UINT64(18ull, scc_svec_size(svec));
+    scc_svec_impl_base(svec)->sc_size = 0ull;
     scc_svec_free(svec);
 }
