527c1ce09f618625cb3c96020ca4ced1285eacb3
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-27 16:00:29 +0100

    Add basic insertion test

--- test/unit/scc_rbtree_test.c
+++ test/unit/scc_rbtree_test.c
@@ -16,3 +16,16 @@ void test_scc_rbtree_size(void) {
     TEST_ASSERT_EQUAL_UINT64(0u, scc_rbtree_size(tree));
     scc_rbtree_free(tree);
 }
+
+void test_scc_rbtree_insert(void) {
+    int *handle = scc_rbtree_init(int, compare);
+    for(int i = 0; i < 3200; i++) {
+        TEST_ASSERT_TRUE(scc_rbtree_insert(handle, i));
+        TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_rbtree_size(handle));
+        for(int j = 0; j <= i; j++) {
+            TEST_ASSERT_FALSE(scc_rbtree_insert(handle, j));
+        }
+    }
+    scc_rbtree_free(handle);
+}
+
