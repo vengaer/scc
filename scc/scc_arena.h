a607667966d3db645e3aa207d239c25b993fda70
Vilhelm <vilhelm.engstrom@tuta.io>
2021-11-27 11:11:25 +0100

    Add arena allocation and deallocation

--- scc/scc_arena.h
+++ scc/scc_arena.h
@@ -1,27 +1,65 @@
 #ifndef SCC_ARENA_H
 #define SCC_ARENA_H
 
+#include "scc_mem.h"
+
+#include <stddef.h>
+
+#ifndef SCC_ARENA_CHUNKSIZE
+enum { SCC_ARENA_CHUNKSIZE = 256 };
+#endif
+
 struct scc_arena {
-    struct scc_chunk *ar_first;
-    struct scc_chunk *ar_current;
+    struct scc_chunk *ar_first;     /* First chunk */
+    struct scc_chunk *ar_current;   /* Current (last) chunk */
+    unsigned short ar_baseoff;      /* Buffer offset in chunk */
+    unsigned short ar_elemsize;     /* Size of element in chunk buffer */
+    unsigned short ar_chunksize;    /* Number of elements in each chunk */
 };
 
 struct scc_chunk {
-    unsigned ch_refcount;
-    unsigned ch_offset;
-    struct scc_chunk *ch_next;
+    unsigned ch_refcount;           /* Number of non-freed elements in this chunk */
+    unsigned ch_offset;             /* Offset of next element relative address of chunk */
+    unsigned ch_end;                /* Offset of last element relative address of chunk */
+    struct scc_chunk *ch_next;      /* Next chunk */
     unsigned char ch_buffer[];
 };
 
-#define scc_arena_init() (struct scc_arena){ 0 };
+#define scc_chunk_impl_layout(type)                                 \
+    struct {                                                        \
+        unsigned ch_refcount;                                       \
+        unsigned ch_offset;                                         \
+        unsigned ch_end;                                            \
+        struct scc_chunk *ch_next;                                  \
+        type ch_buffer[];                                           \
+    }
+
+#define scc_arena_impl_baseoff(type)                                \
+    offsetof(scc_chunk_impl_layout(type), ch_buffer)
+
+#define scc_arena_init(type)                                        \
+    (struct scc_arena){                                             \
+        .ar_baseoff = scc_arena_impl_baseoff(type),                 \
+        .ar_elemsize = sizeof(type),                                \
+        .ar_chunksize = SCC_ARENA_CHUNKSIZE                         \
+    }
+
 void scc_arena_release(struct scc_arena *arena);
 
-#define scc_arena_foreach_chunk(iter, arena)            \
-    for(iter = arena->ar_first; iter <= arena->ar_current; iter = iter->ch_next)
+#define scc_arena_foreach_chunk(iter, arena)                        \
+    for(iter = arena->ar_first; iter; iter = iter->ch_next)
 
-#define scc_arena_foreach_chunk_safe(iter, hare, arena) \
-    for(iter = arena->ar_first, hare = iter->ch_next;   \
-        iter <= arena->ar_current;                      \
+#define scc_arena_foreach_chunk_safe(iter, hare, arena)             \
+    for(iter = arena->ar_first, hare = iter ? iter->ch_next : 0;    \
+        iter;                                                       \
         iter = hare, hare = hare ? hare->ch_next : 0)
+
+#define scc_arena_foreach_chunk_lagging(iter, tortoise, arena)      \
+    for(iter = arena->ar_first, tortoise = 0;                       \
+        iter;                                                       \
+        tortoise = iter, iter = iter->ch_next)
+
+void *scc_arena_alloc(struct scc_arena *arena);
+void scc_arena_free(struct scc_arena *restrict arena, void const *restrict addr);
 
 #endif /* SCC_ARENA_H */
--- src/scc_arena.c
+++ src/scc_arena.c
@@ -1,6 +1,32 @@
 #include <scc/scc_arena.h>
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
+
+static struct scc_chunk *scc_chunk_new(size_t chunksize, size_t elemsize, size_t baseoff);
+static bool scc_chunk_contains_addr(struct scc_chunk const *chunk, void const *addr);
+
+static struct scc_chunk *scc_chunk_new(size_t chunksize, size_t elemsize, size_t baseoff) {
+    size_t const size = chunksize * elemsize + baseoff;
+    struct scc_chunk *chunk = malloc(size);
+    if(!chunk) {
+        return 0;
+    }
+    /* Assume chunk is to be used immediately */
+    chunk->ch_refcount = 1;
+    /* Offset of second element */
+    chunk->ch_offset = baseoff;
+    /* Offset of element after last in chunk */
+    chunk->ch_end = chunk->ch_offset - elemsize + chunksize * elemsize;
+    chunk->ch_next = 0;
+    return chunk;
+}
+
+static inline bool scc_chunk_contains_addr(struct scc_chunk const *chunk, void const *addr) {
+    return addr <= (void const *)((unsigned char const *)chunk + chunk->ch_end) &&
+           (void const *)chunk < addr;
+}
 
 void scc_arena_release(struct scc_arena *arena) {
     struct scc_chunk *iter, *hare;
@@ -8,3 +34,62 @@ void scc_arena_release(struct scc_arena *arena) {
         free(iter);
     }
 }
+
+void *scc_arena_alloc(struct scc_arena *arena) {
+    size_t const chunksize = arena->ar_chunksize;
+    size_t const elemsize = arena->ar_elemsize;
+    size_t const baseoff = arena->ar_baseoff;
+    struct scc_chunk *chunk;
+
+    if(!arena->ar_current) {
+        /* No chunks in arena */
+        chunk = scc_chunk_new(chunksize, elemsize, baseoff);
+        if(!chunk) {
+            return 0;
+        }
+
+        arena->ar_current = chunk;
+        arena->ar_first = chunk;
+    }
+    else if(arena->ar_current->ch_offset == arena->ar_current->ch_end) {
+        /* Chunk full */
+        chunk = scc_chunk_new(chunksize, elemsize, baseoff);
+        if(!chunk) {
+            return 0;
+        }
+        arena->ar_current->ch_next = chunk;
+        arena->ar_current = chunk;
+    }
+    else {
+        chunk = arena->ar_current;
+        ++chunk->ch_refcount;
+        chunk->ch_offset += elemsize;
+    }
+
+    return (unsigned char *)chunk + chunk->ch_offset;
+}
+
+void scc_arena_free(struct scc_arena *restrict arena, void const *restrict addr) {
+    struct scc_chunk *iter, *tortoise;
+    scc_arena_foreach_chunk_lagging(iter, tortoise, arena) {
+        if(scc_chunk_contains_addr(iter, addr)) {
+            break;
+        }
+    }
+    assert(iter);
+    --iter->ch_refcount;
+    if(iter->ch_offset == iter->ch_end && !iter->ch_refcount) {
+        if(!tortoise) {
+            /* First chunk */
+            arena->ar_first = iter->ch_next;
+        }
+        else {
+            tortoise->ch_next = iter->ch_next;
+        }
+        if(iter == arena->ar_current) {
+            arena->ar_current = tortoise;
+        }
+        free(iter);
+    }
+}
+
