073ac23c1372e135f1bb49ef9515ff083325373a
Vilhelm <vilhelm.engstrom@tuta.io>
2021-12-04 13:23:08 +0100

    Avoid recomputing end address in foreach traversal

--- scc/scc_vec.h
+++ scc/scc_vec.h
@@ -2,6 +2,7 @@
 #define SCC_VEC_H
 
 #include "scc_mem.h"
+#include "scc_pp_token.h"
 
 #include <stddef.h>
 
@@ -78,49 +79,52 @@ inline void scc_vec_clear(void *vec) {
 
 void scc_vec_pop_safe(void *vec);
 
-#define scc_vec_init(type)                                          \
-    scc_vec_impl_init(                                              \
-        &scc_vec_impl_nullvec(type),                                \
-        scc_vec_impl_nullsize(type)                                 \
+#define scc_vec_init(type)                                              \
+    scc_vec_impl_init(                                                  \
+        &scc_vec_impl_nullvec(type),                                    \
+        scc_vec_impl_nullsize(type)                                     \
     )
 
-#define scc_vec_from(type, ...)                                     \
-    scc_vec_impl_from(                                              \
-        scc_vec_impl_nullsize(type),                                \
-        (type[]){ __VA_ARGS__ },                                    \
-        scc_arrsize(((type[]){ __VA_ARGS__ })),                     \
-        sizeof(type)                                                \
+#define scc_vec_from(type, ...)                                         \
+    scc_vec_impl_from(                                                  \
+        scc_vec_impl_nullsize(type),                                    \
+        (type[]){ __VA_ARGS__ },                                        \
+        scc_arrsize(((type[]){ __VA_ARGS__ })),                         \
+        sizeof(type)                                                    \
     )
 
-#define scc_vec_reserve(vec, capacity)                              \
+#define scc_vec_reserve(vec, capacity)                                  \
     scc_vec_impl_reserve(&(vec), capacity, sizeof(*(vec)))
 
-#define scc_vec_resize(vec, size)                                   \
+#define scc_vec_resize(vec, size)                                       \
     scc_vec_impl_resize(&(vec), size, sizeof(*(vec)))
 
-#define scc_vec_erase(vec, index)                                   \
+#define scc_vec_erase(vec, index)                                       \
     scc_vec_impl_erase(vec, index, sizeof(*(vec)))
 
-#define scc_vec_erase_range(vec, first, end)                        \
+#define scc_vec_erase_range(vec, first, end)                            \
     scc_vec_impl_erase_range(vec, first, end, sizeof(*(vec)))
 
-#define scc_vec_push(vec, element)                                  \
-    (scc_vec_impl_push_ensure_capacity(&(vec), sizeof(*(vec))) &&   \
+#define scc_vec_push(vec, element)                                      \
+    (scc_vec_impl_push_ensure_capacity(&(vec), sizeof(*(vec))) &&       \
     (vec[scc_vec_impl_base(vec)->sc_size++] = element,1))
 
-#define scc_vec_at(vec, index)                                      \
+#define scc_vec_at(vec, index)                                          \
     (scc_vec_impl_at_check(vec, index),vec[index])
 
-#define scc_vec_foreach(iter, vec)                                  \
+#define scc_vec_foreach(iter, vec)                                      \
     scc_vec_foreach_by(iter, vec, 1)
 
-#define scc_vec_foreach_reversed(iter, vec)                         \
+#define scc_vec_foreach_reversed(iter, vec)                             \
     scc_vec_foreach_reversed_by(iter, vec, 1)
 
-#define scc_vec_foreach_by(iter, vec, by)                           \
-    for(iter = vec; iter < &vec[scc_vec_size(vec)]; iter += by)
+#define scc_vec_foreach_by(iter, vec, by)                               \
+    for(void const *scc_pp_cat_expand(scc_vec_end,__LINE__) =           \
+            (iter = vec, &vec[scc_vec_size(vec)]);                      \
+        (void const *)iter < scc_pp_cat_expand(scc_vec_end,__LINE__);   \
+        iter += by)
 
-#define scc_vec_foreach_reversed_by(iter, vec, by)                  \
+#define scc_vec_foreach_reversed_by(iter, vec, by)                      \
     for(iter = &vec[scc_vec_size(vec) - 1u]; iter >= vec; iter -= by)
 
 #endif /* SCC_VEC_H */
