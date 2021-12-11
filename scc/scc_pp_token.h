85906e9ed5eef7816342bd2563540843d8112cf3
Vilhelm <vilhelm.engstrom@tuta.io>
2021-12-11 16:48:10 +0100

    Allow for specifying underlying stack container

--- scc/scc_pp_token.h
+++ scc/scc_pp_token.h
@@ -7,4 +7,6 @@
 #define scc_pp_cat(x,y) x ## y
 #define scc_pp_cat_expand(x,y) scc_pp_cat(x,y)
 
+#define scc_pp_expand(...) __VA_ARGS__
+
 #endif /* SCC_PP_TOKEN_H */
--- scc/scc_stack.h
+++ scc/scc_stack.h
@@ -1,18 +1,23 @@
-#ifndef SCC_STACH_H
-#define SCC_STACH_H
+#ifndef SCC_STACK_H
+#define SCC_STACK_H
 
+#include "scc_pp_token.h"
 #include "scc_svec.h"
 
-#define scc_stack(type) scc_svec(type)
+#ifndef SCC_STACK_CONTAINER
+#define SCC_STACK_CONTAINER scc_svec
+#endif
 
-#define scc_stack_init(type) scc_svec_init(type)
-#define scc_stack_free(stack) scc_svec_free(stack)
-#define scc_stack_push(stack, value) scc_svec_push(stack, value)
-#define scc_stack_pop(stack) scc_svec_pop(stack)
-#define scc_stack_top(stack) stack[scc_svec_size(stack) - 1]
-#define scc_stack_size(stack) scc_svec_size(stack)
-#define scc_stack_capacity(stack) scc_svec_capacity(stack)
-#define scc_stack_clear(stack) scc_svec_clear(stack)
-#define scc_stack_reserve(stack, capacity) scc_svec_reserve(stack, capacity)
+#define scc_stack(type) scc_pp_expand(SCC_STACK_CONTAINER)(type)
 
-#endif /* SCC_STACH_H */
+#define scc_stack_init(type) scc_pp_cat_expand(SCC_STACK_CONTAINER,_init)(type)
+#define scc_stack_free(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_free)(stack)
+#define scc_stack_push(stack, value) scc_pp_cat_expand(SCC_STACK_CONTAINER,_push)(stack, value)
+#define scc_stack_pop(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_pop)(stack)
+#define scc_stack_top(stack) stack[scc_pp_cat_expand(SCC_STACK_CONTAINER,_size)(stack) - 1]
+#define scc_stack_size(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_size)(stack)
+#define scc_stack_capacity(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_capacity)(stack)
+#define scc_stack_clear(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_clear)(stack)
+#define scc_stack_reserve(stack, capacity) scc_pp_cat_expand(SCC_STACK_CONTAINER,_reserve)(stack, capacity)
+
+#endif /* SCC_STACK_H */
