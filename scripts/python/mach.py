7f9298e97ee6e4f3c6a9e4cdf78fbc7bde6855e2
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-07 14:07:01 +0100

    Detect number of cores while building

--- scripts/python/mach.py
+++ scripts/python/mach.py
@@ -3,6 +3,7 @@
 ''' Script generating machine information in mk format '''
 
 import argparse
+import multiprocessing
 import platform
 
 from wrhandle import FileWrHandle, StdoutWrHandle
@@ -25,6 +26,10 @@ def main(outfile):
         rel = platform.release()
         ver = platform.version().replace('#', '\\#')
         abi = abis.get(f'{system.lower()}_{machine.lower()}', unknown)
+        try:
+            cores = multiprocessing.cpu_count()
+        except NotImplementedError:
+            cores = 1
 
         handle.writeln(f'arch       := {machine.upper() if machine != "" else unknown}')
         handle.writeln(f'bitarch    := {bits.replace("bit","") if bits != "" else unknown}')
@@ -33,6 +38,7 @@ def main(outfile):
         handle.writeln(f'hostosrel  := {rel.upper() if rel != "" else unknown}')
         handle.writeln(f'hostosver  := {ver.upper() if ver != "" else unknown}')
         handle.writeln(f'abi        := {abi}')
+        handle.writeln(f'cpu_cores  := {cores}')
         handle.writeln(f'arch_lower := {machine.lower() if machine != "" else unknown.lower()}')
         handle.writeln(f'abi_lower  := {abi.lower()}')
 
