e650f55d742302628e003904001aca6fc4658fbb
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-07 15:20:22 +0100

    Treat libbenchmark_main.a as a target

--- scripts/mk/perf.mk
+++ scripts/mk/perf.mk
@@ -3,6 +3,7 @@ bmbuilddir := $(bmdir)/build
 dirs       += $(bmbuilddir)
 
 bmalib     := $(bmbuilddir)/src/libbenchmark.a
+bmmainalib := $(patsubst %.$(aext),%_main.$(aext),$(bmalib))
 
 bmflags    := -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on
 
@@ -11,6 +12,10 @@ all:
 
 $(bmalib): $(machinfo)
 $(bmalib): $(call reeval,cpu_cores,$(bmalib))
-$(bmalib): $(submodules) | $(bmbuilddir)
+
+$(bmmainalib): $(machinfo)
+$(bmmainalib): $(call reeval,cpu_cores,$(bmalib))
+
+$(bmalib) $(bmmainalib)&: $(submodules) | $(bmbuilddir)
 	$(CMAKE) -E chdir $(bmbuilddir) $(CMAKE) $(bmflags) $(bmdir)
 	$(CMAKE) --build $(bmbuilddir) --parallel $(cpu_cores) --config Release
