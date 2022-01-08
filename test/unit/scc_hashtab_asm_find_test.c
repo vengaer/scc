e785af1f3675a12c857918056376a6a7b6814c98
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-08 18:26:42 +0100

    Add test verifying the find probing works with vacated collisions

--- test/unit/scc_hashtab_asm_find_test.c
+++ test/unit/scc_hashtab_asm_find_test.c
@@ -45,4 +45,25 @@ void test_scc_hashtab_find_single_collision(void) {
     scc_hashtab_free(tab);
 }
 
+void test_scc_hashtab_find_single_vacated_collision(void) {
+    scc_hashtab(int) tab = scc_hashtab_init(int, eq);
+    struct scc_hashtab *base = scc_hashtab_inspect_base(tab);
+    unsigned long long hash = base->ht_hash(&(int){ 7777 }, sizeof(int));
+    scc_hashtab_metatype *md = scc_hashtab_inspect_md(tab);
+
+    /* "insert" at hashed position */
+    unsigned slot = hash & (base->ht_capacity - 1u);
+    md[slot] = (~(hash >> 50) & 0x3fffu) | SCC_HASHTAB_INSPECT_OCCUPIED;
+
+    TEST_ASSERT_TRUE(scc_hashtab_insert(tab, 7777));
+
+    /* "remove" at hashed position */
+    md[slot] |= SCC_HASHTAB_INSPECT_VACATED;
+
+    long long index = scc_hashtab_impl_find_probe(base, tab, sizeof(int), hash);
+    /* Find should still detect element after removal of colliding one */
+    TEST_ASSERT_EQUAL_INT64(slot + 1ll, index);
+    scc_hashtab_free(tab);
+}
+
 #endif
