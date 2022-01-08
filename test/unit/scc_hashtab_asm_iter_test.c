7aa45618b69f0499f51b41ca9eafeba80987d009
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-08 12:00:14 +0100

    Add iter prep for 32x vector size

--- test/unit/scc_hashtab_asm_iter_test.c
+++ test/unit/scc_hashtab_asm_iter_test.c
@@ -88,4 +88,28 @@ void test_hashtab_prep_iter_collapse_4x_vector(void) {
     }
 }
 
+void test_hashtab_prep_iter_collapse_32x_vector(void) {
+    scc_hashtab_metatype md[32 * SCC_VECSIZE];
+    static scc_hashtab_metatype const ctrl[] = {
+        SCC_HASHTAB_INSPECT_VACANT, SCC_HASHTAB_INSPECT_OCCUPIED,
+        SCC_HASHTAB_INSPECT_GUARD,  SCC_HASHTAB_INSPECT_VACATED
+    };
+    for(unsigned i = 0u; i < scc_arrsize(md); ++i) {
+        md[i] = ctrl[(i + 2) & (scc_arrsize(ctrl) - 1u)];
+    }
+    scc_hashtab_impl_prep_iter(md, scc_arrsize(md));
+    for(unsigned i = 0u; i < scc_arrsize(md); i += scc_arrsize(ctrl)) {
+        TEST_ASSERT_EQUAL_UINT16(0, md[i]);
+    }
+    for(unsigned i = 1u; i < scc_arrsize(md); i += scc_arrsize(ctrl)) {
+        TEST_ASSERT_EQUAL_UINT16(0, md[i]);
+    }
+    for(unsigned i = 2u; i < scc_arrsize(md); i += scc_arrsize(ctrl)) {
+        TEST_ASSERT_EQUAL_UINT16(0, md[i]);
+    }
+    for(unsigned i = 3u; i < scc_arrsize(md); i += scc_arrsize(ctrl)) {
+        TEST_ASSERT_EQUAL_UINT16(1, md[i]);
+    }
+}
+
 #endif
