89031eb5e964a2981a3318d4a78e3185cdb3a313
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-08 11:49:27 +0100

    Add iteration prep test for all occupied

--- test/unit/scc_hashtab_asm_iter_test.c
+++ test/unit/scc_hashtab_asm_iter_test.c
@@ -7,15 +7,15 @@
 
 extern void scc_hashtab_impl_prep_iter(scc_hashtab_metatype *md, unsigned long long size);
 
-void test_hashtab_perp_iter_collapse(void) {
+void test_hashtab_prep_iter_collapse_interleaved(void) {
     scc_hashtab_metatype md[SCC_VECSIZE];
     static scc_hashtab_metatype const ctrl[] = {
         0x00, 0x8000, 0x4000, 0xc000
     };
-    for(unsigned i = 0u; i < SCC_VECSIZE; ++i) {
+    for(unsigned i = 0u; i < scc_arrsize(md); ++i) {
         md[i] = ctrl[i & (scc_arrsize(ctrl) - 1u)];
     }
-    scc_hashtab_impl_prep_iter(md, SCC_VECSIZE);
+    scc_hashtab_impl_prep_iter(md, scc_arrsize(md));
 
     for(unsigned i = 0u; i < SCC_VECSIZE; i += scc_arrsize(ctrl)) {
         TEST_ASSERT_EQUAL_UINT16(0, md[i]);
@@ -28,6 +28,17 @@ void test_hashtab_perp_iter_collapse(void) {
     }
     for(unsigned i = 3u; i < SCC_VECSIZE; i += scc_arrsize(ctrl)) {
         TEST_ASSERT_EQUAL_UINT16(0, md[i]);
+    }
+}
+
+void test_hashtab_prep_iter_collapse_all_occupied(void) {
+    scc_hashtab_metatype md[SCC_VECSIZE];
+    for(unsigned i = 0u; i < scc_arrsize(md); ++i) {
+        md[i] = 0x8000;
+    }
+    scc_hashtab_impl_prep_iter(md, scc_arrsize(md));
+    for(unsigned i = 0u; i < scc_arrsize(md); ++i) {
+        TEST_ASSERT_EQUAL_UINT16(1, md[i]);
     }
 }
 
