bb1ed7958c42604d556f1d16f2c073b95aee042f
Vilhelm <vilhelm.engstrom@tuta.io>
2022-01-08 21:44:30 +0100

    Verify element returned by find

--- test/fuzz/hashtab/hashtab_fuzz_insertion.c
+++ test/fuzz/hashtab/hashtab_fuzz_insertion.c
@@ -9,15 +9,26 @@ bool hashtab_fuzz_insertion(
     size_t unique_end,
     size_t size
 ) {
+    uint32_t const *elem;
     for(unsigned i = 0u; i < unique_end; ++i) {
         if(!scc_hashtab_insert(*tab, data[i])) {
             fprintf(stderr, "Error inserting %" PRIu32 "\n", data[i]);
             return false;
         }
 
+        if(scc_hashtab_size(*tab) != i + 1u) {
+            fprintf(stderr, "Incorrect size on insertion, expected %u, got %zu\n", i + 1u, scc_hashtab_size(*tab));
+            return false;
+        }
+
         for(unsigned j = 0u; j <= i; ++j) {
-            if(!scc_hashtab_find(*tab, data[j])) {
+            elem = scc_hashtab_find(*tab, data[j]);
+            if(!elem) {
                 fprintf(stderr, "%" PRIu32 " lost when inserting %" PRIu32 "\n", data[j], data[i]);
+                return false;
+            }
+            if(*elem != data[j]) {
+                fprintf(stderr, "Incorrect element found on insertion, exptected %" PRIu32 ", got %" PRIu32 "\n", data[j], *elem);
                 return false;
             }
         }
@@ -28,9 +39,18 @@ bool hashtab_fuzz_insertion(
             fprintf(stderr, "Successfully inserted %" PRIu32 " already present in table\n", data[i]);
             return false;
         }
+        if(scc_hashtab_size(*tab) != unique_end) {
+            fprintf(stderr, "Incorrect size on insertion failure, expected %zu, got %zu\n", unique_end, scc_hashtab_size(*tab));
+            return false;
+        }
         for(unsigned j = 0u; j < unique_end; ++j) {
-            if(!scc_hashtab_find(*tab, data[j])) {
+            elem = scc_hashtab_find(*tab, data[j]);
+            if(!elem) {
                 fprintf(stderr, "Lost %" PRIu32 " on insertion failure\n", data[j]);
+                return false;
+            }
+            if(*elem != data[j]) {
+                fprintf(stderr, "Incorrect element found on insertion failure, exptected %" PRIu32 ", got %" PRIu32 "\n", data[j], *elem);
                 return false;
             }
         }
