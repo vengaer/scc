373924b1c6a4b8703808aa5c766cf4327c9a0168
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-01-15 17:59:52 +0100

    Add stack-top and handle spaces in arguments

--- scripts/mk/stack.mk
+++ scripts/mk/stack.mk
@@ -1,7 +1,16 @@
-__stack_top_sym := >>:
+$(if $(mkscripts),,$(error mkscritps is empty))
+include $(mkscripts)/expr.mk
 
-stack-init       = $(eval $(1) := $(__stack_top_sym))
-stack-empty      = $(call not,$(subst $(__stack_top_sym),,$($(1))))
-stack-push       = $(eval $(1) := $(__stack_top_sym)$(2) $(subst $(__stack_top_sym),,$($(1))))
-stack-pop        = $(call assert,$(call not,$(call stack-empty,$(1)))) \
-                   $(eval $(1) := $(__stack_top_sym)$(patsubst $(__stack_top_sym)%,,$($(1))))
+__stack_top_sym        := >>:
+__stack_join_sym       := <<>>
+__space                := $(subst ,, )
+
+__stack-assert-nonempty = $(call assert,$(call not,$(call stack-empty,$(1))),Stack is empty)
+
+stack-init              = $(eval $(1) := $(__stack_top_sym))
+stack-empty             = $(call not,$(subst $(__stack_top_sym),,$($(1))))
+stack-push              = $(eval $(1) := $(__stack_top_sym)$(subst $(__space),$(__stack_join_sym),$(2)) $(subst $(__stack_top_sym),,$($(1))))
+stack-pop               = $(call __stack-assert-nonempty,$(1)) \
+                          $(eval $(1) := $(__stack_top_sym)$(filter-out $(firstword $($(1))),$($(1))))
+stack-top               = $(strip $(call __stack-assert-nonempty,$(1)) \
+                              $(subst $(__stack_top_sym),,$(subst $(__stack_join_sym), ,$(firstword $($(1))))))
