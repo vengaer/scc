6e81bae8921bc871d55057c2aa12ae4e51abbe4e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-01-15 20:16:34 +0100

    Add tests for graph traversal

--- test/buildsystem/make.py
+++ test/buildsystem/make.py
@@ -1,19 +1,19 @@
 import subprocess
 import tempfile
 
-def make(path):
+def make(path, args=None):
+    cmd = ['make', '-f', path]
+    if args is not None:
+        cmd += args.split()
     ''' Run make on the makefile at path. Return exit status, stdout and stderr '''
-    proc = subprocess.run(['make', '-f', path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     decode_stream = lambda b: b.decode('ascii').strip().split('\n')
     return proc.returncode, decode_stream(proc.stdout), decode_stream(proc.stderr)
 
-def make_supplied(contents):
+def make_supplied(contents, args=None, directory=None):
     ''' Create a temporary file, write contents to it and run make on it '''
-    with tempfile.NamedTemporaryFile(mode='w', encoding='ascii', suffix='.mk') as handle:
+    with tempfile.NamedTemporaryFile(mode='w', dir=directory, encoding='ascii', suffix='.mk') as handle:
         handle.file.write('\n'.join(contents))
         handle.file.write('\n')
         handle.file.flush()
-        return make(handle.name)
+        return make(handle.name, args)
---
+++ test/buildsystem/test_graph.py
@@ -0,0 +1,150 @@
+''' Tests related to directory graph traversal '''
+
+import pathlib
+import tempfile
+import pytest
+
+import make
+from fixtures import project_root, script_dir
+
+def _group_debug_output(stdout, maxdepth, root):
+    ''' Group debug output after directory '''
+    dbg = list(filter(lambda s: s.startswith('DEBUG:'), stdout))
+    stack = [root]
+    grouped = {}
+    grouped[stack[-1]] = []
+    for line in dbg:
+        line = line.replace('DEBUG: ', '')
+        if 'enter ' in line:
+            stack.append(line.replace('enter ', '').strip())
+            grouped['/'.join(stack)] = []
+        elif 'exit ' in line:
+            node = line.replace('exit ', '').strip()
+            assert stack[-1] == node
+            stack.pop()
+
+        grouped['/'.join(stack)].append(line)
+
+        # Depth should be at most maxdepth
+        assert len(stack) <= maxdepth
+    return grouped
+
+@pytest.mark.parametrize('explicit_builddir', [False, True])
+def test_flat_graph_traversal(script_dir, explicit_builddir):
+    ''' Create a directory tree with Makefiles and verify that its traversed correctly '''
+    subdirs = ['a', 'b', 'c', 'd']
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for subdir in subdirs:
+            path = pathlib.Path(tmpdir) / subdir
+            path.mkdir()
+            makefile = path / 'Makefile'
+            makefile.touch()
+
+        ec, stdout, stderr = make.make_supplied([
+           f'root      := {tmpdir}',
+            'builddir  := ${root}/build',
+           f'mkscripts := {script_dir}',
+            'include $(mkscripts)/node.mk',
+           f'$(foreach __s,{" ".join(subdirs)},$(call __include-node,$(__s){", build_$(__s)" if explicit_builddir else ""}))',
+            '.PHONY: all',
+            'all:'
+        ], args='DEBUG=1', directory=tmpdir)
+
+        assert ec == 0
+
+        grouped = _group_debug_output(stdout, 2, tmpdir)
+
+        # Verify automatic variables
+        for subdir in subdirs:
+            build_basename = subdir if not explicit_builddir else f'build_{subdir}'
+            assert f'exit {subdir}' in grouped[tmpdir]
+            assert f'enter {subdir}' in grouped[f'{tmpdir}/{subdir}']
+            assert f'__node_builddir: {tmpdir}/build/{build_basename}' in grouped[f'{tmpdir}/{subdir}']
+            assert f'__node_path: {tmpdir}/{subdir}' in grouped[f'{tmpdir}/{subdir}']
+
+        # Should occur once for each time the root is returned to
+        for info in ['__node_path', '__node_builddir']:
+            assert len(list(filter(lambda s: f'{info}:' in s, grouped[tmpdir]))) == len(subdirs)
+
+        # Automatic variables should be the same for the root each time
+        assert len(set(grouped[tmpdir])) == len(grouped[tmpdir]) / len(subdirs) + len(subdirs) - 1
+
+def test_nested_graph_traversal(script_dir):
+    ''' Generate a nested directory graph and verify the correctness '''
+
+    lvl1_dirs = ['a', 'b', 'c', 'd']
+    lvl2_dirs = ['e', 'f', 'g', 'h']
+    lvl3_dirs = ['i', 'j', 'k', 'l']
+
+    def write_makefile(path, contents):
+        with open(str(path / 'Makefile'), 'w', encoding='ascii') as handle:
+            handle.write('\n'.join(contents))
+            handle.write('\n')
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        root = pathlib.Path(tmpdir)
+        for l1d in lvl1_dirs:
+            l1d = root / l1d
+            l1d.mkdir()
+            for l2d in lvl2_dirs:
+                l2d = l1d / l2d
+                l2d.mkdir()
+                for l3d in lvl3_dirs:
+                    l3d = l2d / l3d
+                    l3d.mkdir()
+                    (l3d / 'Makefile').touch()
+                write_makefile(l2d, [ f'$(foreach __s,{" ".join(lvl3_dirs)},$(call __include-node, $(__s)))' ])
+            write_makefile(l1d, [ f'$(foreach __s,{" ".join(lvl2_dirs)},$(call __include-node, $(__s)))' ])
+
+        ec, stdout, stderr = make.make_supplied([
+           f'root      := {tmpdir}',
+            'builddir  := ${root}/build',
+           f'mkscripts := {script_dir}',
+            'include $(mkscripts)/node.mk',
+           f'$(foreach __s,{" ".join(lvl1_dirs)},$(call __include-node,$(__s)))',
+            '.PHONY: all',
+            'all:'
+        ], args='DEBUG=1', directory=tmpdir)
+
+        assert ec == 0
+        grouped = _group_debug_output(stdout, 4, tmpdir)
+
+        path = pathlib.Path(tmpdir)
+        # Verify automatic variables
+        for l1d in lvl1_dirs:
+            l1d = path / l1d
+            assert f'enter {l1d.name}' in grouped[str(l1d)]
+            assert f'__node_builddir: {tmpdir}/build/{l1d.name}' in grouped[str(l1d)]
+            assert f'__node_path: {tmpdir}/{l1d.name}' in grouped[str(l1d)]
+
+            for l2d in lvl2_dirs:
+                l2d = l1d / l2d
+                assert f'enter {l2d.name}' in grouped[str(l2d)]
+                assert f'__node_builddir: {tmpdir}/build/{l1d.name}/{l2d.name}' in grouped[str(l2d)]
+                assert f'__node_path: {tmpdir}/{l1d.name}/{l2d.name}' in grouped[str(l2d)]
+
+                for l3d in lvl3_dirs:
+                    l3d = l2d / l3d
+                    assert f'enter {l3d.name}' in grouped[str(l3d)]
+                    assert f'__node_builddir: {tmpdir}/build/{l1d.name}/{l2d.name}/{l3d.name}' in grouped[str(l3d)]
+                    assert f'__node_path: {tmpdir}/{l1d.name}/{l2d.name}/{l3d.name}' in grouped[str(l3d)]
+
+                for info in ['__node_path', '__node_builddir']:
+                    # Should occur once for __exit_node and once for each __enter_node
+                    assert len(list(filter(lambda s: f'{info}:' in s, grouped[str(l2d)]))) == len(lvl3_dirs) + 1
+
+                # Automatic variables should be the same each time
+                assert len(set(grouped[str(l2d)])) == len(grouped[str(l2d)]) / len(lvl3_dirs) + len(lvl3_dirs)
+
+            for info in ['__node_path', '__node_builddir']:
+                # Should occur once for __exit_node and once for each __enter_node
+                assert len(list(filter(lambda s: f'{info}:' in s, grouped[str(l1d)]))) == len(lvl2_dirs) + 1
+            # Automatic variables should be the same each time
+            assert len(set(grouped[str(l1d)])) == len(grouped[str(l1d)]) / len(lvl2_dirs) + len(lvl2_dirs)
+
+            for info in ['__node_path', '__node_builddir']:
+                # No __enter_node for root
+                assert len(list(filter(lambda s: f'{info}:' in s, grouped[tmpdir]))) == len(lvl1_dirs)
+            # No __enter_node for root
+            assert len(set(grouped[str(l1d)])) == len(grouped[str(l1d)]) / len(lvl2_dirs) + len(lvl2_dirs)
