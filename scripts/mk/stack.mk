1db6d878c13f2a76e60801bd1fb2ef7aafe15523
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-01-15 18:44:11 +0100

    Strip stack name

--- scripts/mk/stack.mk
+++ scripts/mk/stack.mk
@@ -11,11 +11,11 @@ __space                := $(subst ,, )
 __stack-assert-nonempty = $(call assert,$(call not,$(call stack-empty,$(1))),Stack is empty)
 
 stack-init              = $(eval $(1) := $(__stack_top_sym))
-stack-empty             = $(call not,$(subst $(__stack_top_sym),,$($(1))))
-stack-push              = $(if $(2),$(eval $(1) := $(__stack_top_sym)$(subst $(__space),$(__stack_join_sym),$(strip $(2))) $(subst $(__stack_top_sym),,$($(1)))))
+stack-empty             = $(call not,$(subst $(__stack_top_sym),,$($(strip $(1)))))
+stack-push              = $(if $(2),$(eval $(1) := $(__stack_top_sym)$(subst $(__space),$(__stack_join_sym),$(strip $(2))) $(subst $(__stack_top_sym),,$($(strip $(1))))))
 stack-pop               = $(call __stack-assert-nonempty,$(1)) \
-                          $(eval $(1) := $(__stack_top_sym)$(filter-out $(firstword $($(1))),$($(1))))
+                          $(eval $(1) := $(__stack_top_sym)$(filter-out $(firstword $($(strip $(1)))),$($(strip $(1)))))
 stack-top               = $(strip $(call __stack-assert-nonempty,$(1)) \
-                              $(subst $(__stack_top_sym),,$(subst $(__stack_join_sym), ,$(firstword $($(1))))))
+                              $(subst $(__stack_top_sym),,$(subst $(__stack_join_sym), ,$(firstword $($(strip $(1)))))))
 
 endif # __Stack_mk
