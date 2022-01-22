d0e336b64362464103d545ed9b2682ceb5f3c2ce
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-01-22 18:40:52 +0100

    Add dstack-add

--- scripts/mk/stack.mk
+++ scripts/mk/stack.mk
@@ -11,12 +11,29 @@ __space                := $(subst ,, )
 
 __stack-assert-nonempty = $(call assert,$(call not,$(call stack-empty,$(1))),Stack is empty)
 
-stack-init              = $(eval $(1) := $(__stack_top_sym))
+stack-init              = $(eval $(1) := $(__stack_top_sym)$(2))
 stack-empty             = $(call not,$(subst $(__stack_top_sym),,$($(strip $(1)))))
 stack-push              = $(eval $(1) := $(__stack_top_sym)$(subst $(__space),$(__stack_join_sym),$(strip $(if $(2),$(2),$(__stack_empty_ent)))) $(subst $(__stack_top_sym),,$($(strip $(1)))))
 stack-pop               = $(call __stack-assert-nonempty,$(1)) \
                           $(eval $(1) := $(__stack_top_sym)$(filter-out $(firstword $($(strip $(1)))),$($(strip $(1)))))
 stack-top               = $(strip $(call __stack-assert-nonempty,$(1)) \
                               $(subst $(__stack_empty_ent),,$(subst $(__stack_top_sym),,$(subst $(__stack_join_sym), ,$(firstword $($(strip $(1))))))))
+
+__dstack_add_prefix   := ||+||
+
+dstack-init             = $(call stack-init,__dstack_$(1))$(eval __dstack_$(1)_top := $(2))
+dstack-top              = $(__dstack_$(1)_top)
+dstack-dump             = $(info $(__dstack_$(1)))
+
+define dstack-add
+$(strip
+    $(eval __dstack_diff :=)
+    $(foreach __w,$(2),
+        $(if $(call not,$(findstring $(__w),$(__dstack_$(1)_top))),
+            $(eval __dstack_$(1)_top += $(__w))
+            $(eval __dstack_diff += $(__w))))
+    $(if $(__dstack_diff),
+        $(call stack-push,__dstack_$(1),$(__dstack_add_prefix)$(subst $(__space),$(__stack_join_sym),$(strip $(__dstack_diff))))))
+endef
 
 endif # __Stack_mk
