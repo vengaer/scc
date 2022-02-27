fad4b2cb6a6ce01f0e6c581ea534933c6e4daa61
Vilhelm <vilhelm.engstrom@tuta.io>
2022-02-27 09:02:03 +0100

    Initialize submodules before linting

--- scripts/mk/lint.mk
+++ scripts/mk/lint.mk
@@ -25,7 +25,7 @@ $(eval
         $(foreach __s,$(wildcard $(__node_path)/*.$(__ext)),
             $(eval __lint_$(__s) := $(__node_builddir)/.lint.$(notdir $(__s)).stamp)
 
-            $(__lint_$(__s)): $(__s) $(__all_mkfiles) | $(__node_builddir)
+            $(__lint_$(__s)): $(__s) $(__all_mkfiles) | $(__node_builddir) $(submodules)
 	            $$(info [TIDY] $$(notdir $$<))
 	            $(TIDY) $(TIDYFLAGS) $$< -- $(CPPFLAGS) $(CFLAGS)
 	            $(TOUCH) $$@
