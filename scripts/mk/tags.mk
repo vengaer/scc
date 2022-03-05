685da08dcceb98a64ddf1f19d80fb07bb9ad10e5
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-05 22:02:46 +0100

    build: Add header guard for tags makefile

--- scripts/mk/tags.mk
+++ scripts/mk/tags.mk
@@ -1,5 +1,10 @@
+ifndef __Tags_mk
+__Tags_mk := _
+
 CTAGS      := ctags
 tagsexcl   := .git build docs submodules/benchmark/build
 CTAGSFLAGS := -R $(addprefix --exclude=,$(tagsexcl))
 
 tags: $(submodules)
+
+endif # __Tags_mk
