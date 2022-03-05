7f02c068b556abc0c89c43493f2a3530a991010c
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-05 13:55:29 +0100

    asm: Use macros to shorten function declarations

--- src/asm/x86_64/sysv/asm.h
+++ src/asm/x86_64/sysv/asm.h
@@ -1,6 +1,12 @@
 #ifndef ASM_H
 #define ASM_H
 
-long long scc_hashtab_probe_insert(void *base, void *handle, unsigned long long elemsize, unsigned long long hash);
+#define i64 long long
+#define u64 unsigned long long
+
+i64 scc_hashtab_probe_insert(void *base, void *handle, u64 elemsize, u64 hash);
+
+#undef u64
+#undef i64
 
 #endif /* ASM_H */
