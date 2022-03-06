8cf0f912aa8b1ef81b43a57f455e0182c3ff644a
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-06 10:42:21 +0100

    hashtab: Add test verifying that probing detects the $very last empty slot

--- test/unit/hashtab/scc_hashtab_avx2_test.c
+++ test/unit/hashtab/scc_hashtab_avx2_test.c
@@ -63,23 +63,23 @@ void test_insertion_probe_functional_up_to_full_capacity(void) {
     scc_hashtab_free(tab);
 }
 
-/* TODO: Probably too stringent, assuming the table is never
- *       completely full seems reasonable. This does expose
- *       an issue though. In the event of SCC_VECSIZE consecutively
- *       occupied slots where the hash of the value to be inserted
- *       does not match any of the SCC_VECSIZE metadata entries,
- *       scc_hashtab_probe_insert assumes slot 0 of the vector is
- *       vacant.
- */
-void test_insertion_probe_rejects_at_full_capacity(void) {
+void test_insertion_probe_finds_single_vacant(void) {
     scc_hashtab(int) tab = scc_hashtab_init(int, eq);
     struct scc_hashtab_base *base = scc_hashtab_inspect_base(tab);
-
-    scc_hashtab_metatype *md = scc_hashtab_inspect_metadata(tab);
-    memset(md, 0x80, scc_hashtab_capacity(tab) + scc_hashtab_impl_guardsz());
-    *tab = 32;
     unsigned long long hash = scc_hashtab_fnv1a(tab, sizeof(int));
-    TEST_ASSERT_EQUAL_INT64(-1ll, scc_hashtab_probe_insert(base, tab, sizeof(int), hash));
+    size_t slot = hash & (scc_hashtab_capacity(tab) - 1u);
+    scc_hashtab_metatype *md = scc_hashtab_inspect_metadata(tab);
+    /* Mark all slots as occupied */
+    memset(md, 0x80, scc_hashtab_capacity(tab) + scc_hashtab_impl_guardsz());
+
+    size_t insert_slot = slot ? slot - 1u: scc_hashtab_capacity(tab) - 1u;
+    /* Clear the very last slot to be probed */
+    md[insert_slot] = 0u;
+    if(insert_slot < scc_hashtab_impl_guardsz()) {
+        md[insert_slot + scc_hashtab_capacity(tab)] = 0u;
+    }
+    *tab = 32;
+    TEST_ASSERT_EQUAL_INT64((long long)insert_slot, scc_hashtab_probe_insert(base, tab, sizeof(int), hash));
 
     scc_hashtab_free(tab);
 }
