c95fa637d7e13a36f8130e2ab293e0f83338c550
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-20 12:48:38 +0100

    hashmap: Add initialization and freeing

---
+++ scc/scc_hashmap.h
@@ -0,0 +1,318 @@
+#ifndef SCC_HASHMAP_H
+#define SCC_HASHMAP_H
+
+#include "scc_mem.h"
+
+#include <stddef.h>
+
+/* scc_hashmap_impl_pair
+ *
+ * Internal use only
+ *
+ * Expands to a struct appropriate for storing
+ * pairs of key-value types
+ *
+ * keytype
+ *      The key type to be stored in the map
+ *
+ * valuetype
+ *      The value type to be stored in the map
+ */
+#define scc_hashmap_impl_pair(keytype, valuetype)                                       \
+    struct { keytype key; valuetype value; }
+
+/* scc_hashmap
+ *
+ * Expands to a pointer suitable for
+ * storing a hash map handle for the given
+ * key and value types
+ *
+ * keytype
+ *      The key type to be stored in the map
+ *
+ * valuetype
+ *      The value type to be stored in the map
+ */
+#define scc_hashmap(keytype, valuetype)                                                 \
+    scc_hashmap_impl_pair(keytype, valuetype) *
+
+#define SCC_HASHMAP_IMPL_GUARDSZ ((unsigned)SCC_VECSIZE - 1u)
+
+enum { SCC_HASHMAP_STACKCAP = 32 };
+
+#ifndef SCC_EQ_TYPEDEF
+#define SCC_EQ_TYPEDEF
+/* scc_eq
+ *
+ * Signature of function used for equality comparisons
+ */
+typedef _Bool(*scc_eq)(void const *, void const *);
+#endif /* SCC_EQ_TYPEDEF */
+
+#ifndef SCC_HASH_TYPEDEF
+#define SCC_HASH_TYPEDEF
+/* scc_hash
+ *
+ * Signature of function used for hashing
+ */
+typedef unsigned long long(*scc_hash)(void const*, size_t);
+#endif /* SCC_HASH_TYPEDEF */
+
+typedef unsigned char scc_hashmap_metatype;
+
+/* struct scc_hashmap_base
+ *
+ * Internal use only
+ *
+ * Base struct of the hash map. Exposed only indirectly
+ * through a fat pointer. Given a struct scc_hashmap_base
+ * base, the address of said pointer is obtained by computing
+ * &base.hm_fwoff + base.hm_fwoff + 1.
+ *
+ * scc_eq hm_eq
+ *      Pointer to function used for equality comparison
+ *
+ * scc_hash hm_hash
+ *      Pointer to hash function.
+ *
+ * size_t hm_valoff
+ *      Offset of the value array relative the base address.
+ *      Used to access the values in the FAM part of the struct.
+ *
+ * size_t hm_mdoff
+ *      Offset of metadata array relative the base address. This
+ *      is used to access the metadata in the FAM part of the
+ *      struct.
+ *
+ * size_t hm_size
+ *      Size of the hash map
+ *
+ * size_t hm_capacity
+ *      Capacity of the map. Always a power of 2.
+ *
+ * unsigned char hm_dynalloc
+ *      Set to 1 if the map was allocated dynamically.
+ *      Upon initial construction, the map is allocated on the
+ *      stack. This field is set on the first rehash.
+ *
+ * unsigned char hm_valpad
+ *      Tracks the number of padding bytes between the key and value
+ *      in the internal pair struct.
+ *
+ * unsigned char hm_fwoff
+ *      Offset of the pointer exposed through the API. The offset
+ *      is relative to the field itself.
+ *
+ * unsigned char hm_buffer[]
+ *      FAM hiding type-specific details. For the exact layout,
+ *      refer to scc_hashmap_impl_layout.
+ */
+struct scc_hashmap_base {
+    scc_eq hm_eq;
+    scc_hash hm_hash;
+    size_t hm_valoff;
+    size_t hm_mdoff;
+    size_t hm_size;
+    size_t hm_capacity;
+    unsigned char hm_dynalloc;
+    unsigned char hm_valpad;
+    unsigned char hm_fwoff;
+    unsigned char hm_buffer[];
+};
+
+/* scc_hashmap_impl_layout
+ *
+ * Internal use only
+ *
+ * The actual layout of the hash map instantiated for a given
+ * key-value type pair. The hm_eq through hm_fwoff members are
+ * the same as for struct scc_hashmap_base.
+ *
+ * unsigned char hm_bkoff;
+ *      Field used for tracking the padding between hm_fwoff and
+ *      hm_curr. Just as hm_fwoff is used to compute the address
+ *      of hm_curr given a base address, hm_bkoff is used to
+ *      compute the base address given the address of hm_curr.
+ *
+ *      The primary purpose of this field is to force injection of
+ *      padding bytes between hm_fwoff and hm_curr. Whether the offset
+ *      is actually stored in this field depends on the alignment of
+ *      the type parameter. In practice, this occurs only for 1-byte
+ *      aligned types. For types with stricter alignment requirements,
+ *      the last padding byte is used instead.
+ *
+ * scc_hashmap_impl_pair(keytype, valuetype) hm_curr
+ *      Volatile instance of the pair stored in the hash map. Used
+ *      for temporary storage of values to be inserted or probed
+ *      for in order to provide rvalue support.
+ *
+ * keytype hm_keys[SCC_HASHMAP_STACKCAP]
+ *      The keys array
+ *
+ * valuetype hm_vals[SCC_HASHMAP_STACKCAP]
+ *      The value array
+ *
+ * scc_hashmap_metatype hm_meta[SCC_HASHMAP_STACKCAP]
+ *      Metadata array used for tracking vacant slots. Each slot n
+ *      in the metadata array corresponds to slot n in the data array.
+ *
+ *      A slot is unused if the value of its metadata entry is 0. If the
+ *      byte has a non-zero value with the MSB unset, the slot was previously
+ *      occupied but has since been vacated. Such slots can be reused during
+ *      insertion but cannot be used as probing stops.
+ *
+ *      An entry with the MSB set signifies that the slot is occupied. In
+ *      this case, the remaining bits are the CHAR_BIT - 1 most
+ *      significant bytes of the hash computed for the value in the
+ *      corresponding slot in the data array. This is used for
+ *      avoiding unnecessary calls to eq.
+ *
+ * scc_hashmap_metatype hm_guard[SCC_HASHMAP_IMPL_GUARDSZ];
+ *      Guard to allow for unaligned vector loads without risking
+ *      reads from potential guard pages. The SCC_HASHMAP_IMPL_GUARDSZ
+ *      low bytes of hm_meta are mirrored in the guard.
+ */
+#define scc_hashmap_impl_layout(keytype, valuetype)                                         \
+    struct {                                                                                \
+        scc_eq hm_eq;                                                                       \
+        scc_hash hm_hash;                                                                   \
+        size_t hm_valoff;                                                                   \
+        size_t hm_mdoff;                                                                    \
+        size_t hm_size;                                                                     \
+        size_t hm_capacity;                                                                 \
+        unsigned char hm_dynalloc;                                                          \
+        unsigned char hm_valpad;                                                            \
+        unsigned char hm_fwoff;                                                             \
+        unsigned char hm_bkoff;                                                             \
+        scc_hashmap_impl_pair(keytype, valuetype) hm_curr;                                  \
+        keytype hm_keys[SCC_HASHMAP_STACKCAP];                                              \
+        valuetype hm_vals[SCC_HASHMAP_STACKCAP];                                            \
+        scc_hashmap_metatype hm_meta[SCC_HASHMAP_STACKCAP];                                 \
+        scc_hashmap_metatype hm_guard[SCC_HASHMAP_IMPL_GUARDSZ];                            \
+    }
+
+
+/* scc_hashmap_impl_init
+ *
+ * Internal use only
+ *
+ * Initialize an empty hash map and return a handle to it.
+ *
+ * struct scc_hashmap_base *base
+ *      Address of hash map base. The handle returned by the function
+ *      refers to the hm_curr entry in this table
+ *
+ * size_t coff
+ *      Offset of hm_curr relative the address of *base
+ *
+ * size_t valoff
+ *      Offset of the value type in the key-value pair
+ *
+ * size_t keysize
+ *      Size of the key type
+ */
+void *scc_hashmap_impl_init(struct scc_hashmap_base *base, size_t coff, size_t valoff, size_t keysize);
+
+/* scc_hashmap_init_with_hash
+ *
+ * Initialize hash map with a specified hash function.
+ *
+ * keytype
+ *      The key type the hash map is to store
+ *
+ * valuetype
+ *      The value type the hash map is to store
+ *
+ * scc_eq eq
+ *      Pointer to function to use for equality comparisons
+ *
+ * scc_hash hash
+ *      Pointer to function to use for hashing
+ */
+#define scc_hashmap_init_with_hash(keytype, valuetype, eq, hash)                            \
+    scc_hashmap_impl_init(                                                                  \
+        (void *)&(scc_hashmap_impl_layout(keytype, valuetype)){                             \
+            .hm_eq = eq,                                                                    \
+            .hm_hash = hash,                                                                \
+            .hm_capacity = SCC_HASHMAP_STACKCAP,                                            \
+            .hm_valoff = offsetof(scc_hashmap_impl_layout(keytype, valuetype), hm_vals),    \
+            .hm_mdoff = offsetof(scc_hashmap_impl_layout(keytype, valuetype), hm_meta)      \
+        },                                                                                  \
+        offsetof(scc_hashmap_impl_layout(keytype, valuetype), hm_curr),                     \
+        offsetof(scc_hashmap_impl_pair(keytype, valuetype), value),                         \
+        sizeof(keytype)                                                                     \
+    )
+
+/* scc_hashmap_init
+ *
+ * Initialize a hash map. The hash function is set to an fnv1a.
+ *
+ * keytype
+ *      The key type the hash map is to store
+ *
+ * valuetype
+ *      The value type the hash map is to store
+ *
+ * scc_eq eq
+ *      Pointer to function to use for equality comparisons
+ */
+#define scc_hashmap_init(keytype, valuetype, eq)                                            \
+    scc_hashmap_init_with_hash(keytype, valuetype, eq, scc_hashmap_fnv1a)
+
+/* scc_hashmap_impl_bkpad
+ *
+ * Internal use only
+ *
+ * Compute number of padding bytes between ht_curr and hm_fwoff
+ */
+inline size_t scc_hashmap_impl_bkpad(void const *handle) {
+    return ((unsigned char const *)handle)[-1] + sizeof(((struct scc_hashmap_base *)0)->hm_fwoff);
+}
+
+/* scc_hashmap_impl_base_qual
+ *
+ * Internal use only
+ *
+ * Obtain qualified pointer to the struct scc_hashmap_base
+ * corresponding to the given handle
+ */
+#define scc_hashmap_impl_base_qual(handle, qual)                            \
+    scc_container_qual(                                                     \
+        (unsigned char qual *)(handle) - scc_hashmap_impl_bkpad(handle),    \
+        struct scc_hashmap_base,                                            \
+        hm_fwoff,                                                           \
+        qual                                                                \
+    )
+
+/* scc_hashmap_impl_base
+ *
+ * Internal use only
+ *
+ * Obtain unqualified pointer to the struct scc_hashmap_base
+ * corresponding to the given handle
+ */
+#define scc_hashmap_impl_base(handle)                                       \
+    scc_hashmap_impl_base_qual(handle,)
+
+/* scc_hashmap_fnv1a
+ *
+ * Simple alternative Fowler-Voll-No implementation
+ *
+ * void const *data
+ *      Address of data to be hashed. The data is treated as a
+ *      consecutive array of bytes. Potential padding in structs
+ *      must be explicitly initialized to avoid hash mismatches.
+ *
+ * size_t size
+ *      Size of data, in bytes
+ */
+unsigned long long scc_hashmap_fnv1a(void const *data, size_t size);
+
+
+/* scc_hashmap_free
+ *
+ * Reclaim memory used by the given hash map
+ */
+void scc_hashmap_free(void *map);
+
+#endif /* SCC_HASHMAP_H */
---
+++ src/scc_hashmap.c
@@ -0,0 +1,71 @@
+#include <scc/scc_hashmap.h>
+
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+size_t scc_hashmap_impl_bkpad(void const *handle);
+
+/* scc_hashmap_calcpad
+ *
+ * Calculate the number of padding bytes between hm_fwoff and hm_curr
+ *
+ * size_t coff
+ *      Offset of hm_curr relative the base of the struct
+ */
+static inline unsigned char scc_hashmap_calcpad(size_t coff) {
+    size_t const fwoff = coff -
+        offsetof(struct scc_hashmap_base, hm_fwoff) -
+        sizeof(((struct scc_hashmap_base *)0)->hm_fwoff);
+    assert(fwoff <= UCHAR_MAX);
+    return fwoff;
+}
+
+/* scc_hashmap_set_bkoff
+ *
+ * Set the hm_bkoff field
+ *
+ * void *map
+ *      Handle referring to the hash map
+ *
+ * unsigned char bkoff
+ *      The value to set
+ */
+static inline void scc_hashmap_set_bkoff(void *map, unsigned char bkoff) {
+    ((unsigned char *)map)[-1] = bkoff;
+}
+
+void *scc_hashmap_impl_init(struct scc_hashmap_base *base, size_t coff, size_t valoff, size_t keysize) {
+    size_t const valpad = valoff - keysize;
+    assert(valpad <= UCHAR_MAX);
+    base->hm_valpad = valpad;
+    base->hm_fwoff = scc_hashmap_calcpad(coff);
+    unsigned char *map = (unsigned char *)base + coff;
+    scc_hashmap_set_bkoff(map, base->hm_fwoff);
+    return map;
+}
+
+unsigned long long scc_hashmap_fnv1a(void const *data, size_t size) {
+#define SCC_FNV_OFFSET_BASIS 0xcbf29ce484222325ull
+#define SCC_FNV_PRIME 0x100000001b3ull
+
+    unsigned long long hash = SCC_FNV_OFFSET_BASIS;
+    unsigned char const *dptr = data;
+    for(size_t i = 0; i < size; ++i) {
+        hash ^= dptr[i];
+        hash *= SCC_FNV_PRIME;
+    }
+    return hash;
+
+#undef SCC_FNV_OFFSET_BASIS
+#undef SCC_FNV_PRIME
+}
+
+void scc_hashmap_free(void *map) {
+    struct scc_hashmap_base *base = scc_hashmap_impl_base(map);
+    if(base->hm_dynalloc) {
+        free(base);
+    }
+}
+
