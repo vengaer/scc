452a2b7748ad87138efddc9c3fcf3752e47cb6bd
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-20 11:38:28 +0100

    docs: Use readthedocs's theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,9 @@ project = 'scc'
 copyright = '2022, Vilhelm Engström'
 author = 'Vilhelm Engström'
 
+release = '0.1'
+version = '0.0.1'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,7 +31,18 @@ author = 'Vilhelm Engström'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx'
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -41,4 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
