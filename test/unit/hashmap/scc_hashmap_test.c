8b3f58cd2f0b995d39f9dc6d96c7f3bd3aa341bc
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-26 14:30:05 +0100

    hashmap: Add test verifying that insertion increases size

--- test/unit/hashmap/scc_hashmap_test.c
+++ test/unit/hashmap/scc_hashmap_test.c
@@ -9,7 +9,27 @@ static bool eq(void const *left, void const *right) {
     return *(int const *)left == *(int const *)right;
 }
 
+/* test_scc_hashmap_init
+ *
+ * Initialize a hash map, verify its size and free it
+ */
 void test_scc_hashmap_init(void) {
     scc_hashmap(int, unsigned) map = scc_hashmap_init(int, unsigned, eq);
+    TEST_ASSERT_EQUAL_UINT64(0ull, scc_hashmap_size(map));
+    scc_hashmap_free(map);
+}
+
+/* test_scc_hashmap_insert_changes_size
+ *
+ * Repeatedly insert elements in the hash table and
+ * verify that its size is increased
+ */
+void test_scc_hashmap_insert_changes_size(void) {
+    enum { TESTSIZE = 128 };
+    scc_hashmap(int, unsigned short) map = scc_hashmap_init(int, unsigned short, eq);
+    for(int i = 0u; i < TESTSIZE; ++i) {
+        TEST_ASSERT_TRUE(scc_hashmap_insert(&map, i, (unsigned short)i));
+        TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_hashmap_size(map));
+    }
     scc_hashmap_free(map);
 }
