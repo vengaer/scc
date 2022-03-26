e7b52f5abd62386cfbb34b2af0921920f165c6a5
Vilhelm <vilhelm.engstrom@tuta.io>
2022-03-26 15:21:26 +0100

    hashtab: Remove obsolete inspection constants

--- test/inspect/scc_hashtab_inspect.h
+++ test/inspect/scc_hashtab_inspect.h
@@ -3,11 +3,6 @@
 
 #include <scc/scc_hashtab.h>
 
-#define SCC_HASHTAB_INSPECT_VACANT   ((scc_hashtab_metatype)0x0000)
-#define SCC_HASHTAB_INSPECT_OCCUPIED ((scc_hashtab_metatype)0x8000)
-#define SCC_HASHTAB_INSPECT_GUARD    ((scc_hashtab_metatype)0x4000)
-#define SCC_HASHTAB_INSPECT_VACATED  ((scc_hashtab_metatype)0xc000)
-
 struct scc_hashtab_base *scc_hashtab_inspect_base(void *tab);
 scc_hashtab_metatype *scc_hashtab_inspect_metadata(void *tab);
 
