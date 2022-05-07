2b059aeb9980c46d5cef2895f04315bdbbdd176d
Vilhelm <vilhelm.engstrom@tuta.io>
2022-05-07 10:54:05 +0200

    build: Add macro for deriving relative paths

---
+++ scripts/mk/path.mk
@@ -0,0 +1,24 @@
+ifndef __Path_mk
+__Path_mk := _
+
+include $(mkscripts)/expr.mk
+
+__path_sep := /
+__space    := $(subst ,, )
+
+# Convert absolute path $(1) to a one relative $(2)
+# $(call path-relative-to,START,ABSPATH)
+define path-relative-to
+$(strip
+    $(eval __common_prefix :=)
+    $(foreach __w,$(subst $(__path_sep), ,$(1)),
+        $(if $(findstring |_|$(__common_prefix)$(__path_sep)$(__w),|_|$(2)),
+            $(eval __common_prefix := $(__common_prefix)$(__path_sep)$(__w)),
+          $(eval __common_prefix := $(__common_prefix)|<|)))
+    $(eval __common_prefix := $(firstword $(subst |<|, ,$(__common_prefix))))
+    $(eval __nup := $(words $(subst $(__path_sep), ,$(subst $(__common_prefix),,$(1)))))
+    $(1)$(__path_sep)$(subst $(__space),$(__path_sep),$(call for-in-range,$(__nup),..))$(subst $(__common_prefix),,$(2)))
+endef
+
+endif # __Path_mk
+
