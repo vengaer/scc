1070c26c15a06471c9238658f9f5f502ae46e5d0
Vilhelm <vilhelm.engstrom@tuta.io>
2022-05-08 13:12:54 +0200

    ringdeque: Add test for push_front

--- test/unit/ringdeque/scc_ringdeque_test.c
+++ test/unit/ringdeque/scc_ringdeque_test.c
@@ -20,3 +20,19 @@ void test_scc_ringdeque_push_back(void) {
     }
     scc_ringdeque_free(deque);
 }
+
+void test_scc_ringdeque_push_front(void) {
+    scc_ringdeque(unsigned) deque = scc_ringdeque_init(unsigned);
+    size_t cap = scc_ringdeque_capacity(deque);
+    for(unsigned i = 0u; i < cap + 3u; ++i) {
+        TEST_ASSERT_TRUE(scc_ringdeque_push_front(&deque, i));
+        TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_ringdeque_size(deque));
+    }
+
+    struct scc_ringdeque_base *base = scc_ringdeque_impl_base(deque);
+    for(unsigned i = base->rd_begin, j = cap + 2; i != base->rd_end; i = (i + 1u) & (base->rd_capacity - 1u), --j) {
+        TEST_ASSERT_EQUAL_UINT32(j, deque[i]);
+    }
+
+    scc_ringdeque_free(deque);
+}
