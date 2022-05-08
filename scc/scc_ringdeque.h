b596892b82726736d23c49f2b6a6d98c7a8b4048
Vilhelm <vilhelm.engstrom@tuta.io>
2022-05-08 12:55:51 +0200

    ringdeque: Add push_back support

--- scc/scc_ringdeque.h
+++ scc/scc_ringdeque.h
@@ -94,6 +94,7 @@ struct scc_ringdeque_base {
 //?          Used for tracking potential padding bytes between :c:texpr:`rd_end`
 //?          and :c:texpr:`rd_data`
 //?
+//?     .. _unsigned_char_rd_dynalloc:
 //?     .. c:var:: unsigned char rd_dynalloc
 //?
 //?         Field tracking whether the ringdeque was allocated
@@ -232,5 +233,70 @@ inline size_t scc_ringdeque_capacity(void const *deque) {
 inline size_t scc_ringdeque_size(void const *deque) {
     return scc_ringdeque_impl_base_qual(deque, const)->rd_size;
 }
+
+//? .. c:function:: _Bool scc_ringdeque_impl_prepare_push(void *dequeaddr, size_t elemsize)
+//?
+//?     Ensure that the capacity of the ringdeque at :c:texpr:`*(void **)dequeaddr` is
+//?     large enough that at least one more element can be pushed to the back of the
+//?     collection.
+//?
+//?     If the capacity of the ringdeque is not sufficient at the time of the call,
+//?     the entire collection is reallocated.
+//?
+//?     .. note::
+//?
+//?         Internal use only
+//?
+//?     :param dequeaddr: Address of the handle used to refer to the ringdeque
+//?     :param elemsize: Size of the elements stored in the ringdeque, in bytes
+//?     :returns: A :code:`_Bool` indicating whether the ringdeque capacity
+//?               is sufficiently large
+//?     :retval true: The ringdeque is already sufficiently large to accomodate another
+//?                   push. Should this be the case, :c:texpr:`*(void **)dequeaddr`
+//?                   is not modified.
+//?     :retval true: The ringdeque was successfully reallocated to accomodate the
+//?                   pending push. :c:texpr:`*(void **)dequeaddr` is updated accordingly.
+//?     :retval false: Allocation failure when resizing
+_Bool scc_ringdeque_impl_prepare_push(void *dequeaddr, size_t elemsize);
+
+//? .. c:function:: size_t scc_ringdeque_impl_push_back_index(void *deque)
+//?
+//?     Obtain the index of the slot just beyond the last in the given ringdeque
+//?     and move the :ref:`rd_end <size_t_rd_end>` forward.
+//?
+//?     .. note::
+//?
+//?         Internal use only
+//?
+//?     :param deque: Handle to the ringdeque in question
+//?     :returns: The index of the slot just beyond the last one in the ringdeque
+inline size_t scc_ringdeque_impl_push_back_index(void *deque) {
+    struct scc_ringdeque_base *base = scc_ringdeque_impl_base(deque);
+    size_t index = base->rd_end;
+    base->rd_end = (base->rd_end + 1u) & (base->rd_capacity - 1u);
+    ++base->rd_size;
+    return index;
+}
+
+//! .. c:function:: _Bool scc_ringdeque_push_back(void *dequeaddr, type value)
+//!
+//!     Push :c:texpr:`value` to the back of the ringdeque, reallocating the
+//!     collection if needed.
+//!
+//!     Any pointers into the ringdeque obtained prior to the call should be treated
+//!     as invalid once the function returns.
+//!
+//!     :param dequeaddr: Address of the handle to the ringdeque in question. Should
+//!                       the ringdeque have to be reallocated, :c:texpr:`*(void **)dequeaddr`
+//!                       is updated accordingly.
+//!     :param value: Value to push to the back of the ringdeque. Must be
+//!                   implicitly convertible to the type the ringdeque was
+//!                   instantiated for.
+//!     :returns: A :code:`_Bool` indicating whether the push was successful
+//!     :retval true: The push succeeded
+//!     :retval false: Memory allocation failure
+#define scc_ringdeque_push_back(dequeaddr, value)                               \
+    (scc_ringdeque_impl_prepare_push(dequeaddr, sizeof(**(dequeaddr))) &&    \
+    ((*(dequeaddr))[scc_ringdeque_impl_push_back_index(*(dequeaddr))] = value),1)
 
 #endif /* SCC_RINGDEQUE_H */
--- src/scc_ringdeque.c
+++ src/scc_ringdeque.c
@@ -1,21 +1,111 @@
 #include <scc/scc_ringdeque.h>
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 size_t scc_ringdeque_impl_npad(void const *deque);
 size_t scc_ringdeque_capacity(void const *deque);
 size_t scc_ringdeque_size(void const *deque);
+size_t scc_ringdeque_impl_push_back_index(void *deque);
 
-/* scc_ringdeque_is_allocd
- *
- * Read the rd_dynalloc field
- *
- * void const *deque
- *      Handle to the ringdeque in question
- */
+//? .. c:function:: bool scc_ringdeque_is_allocd(void const *deque)
+//?
+//?     Read the :ref:`rd_dynalloc <unsigned_char_rd_dynalloc>` field
+//?
+//?     .. note::
+//?
+//?         Internal use only
+//?
+//?     :param deque: Handle to the ringdeque in question
+//?     :returns: Value of the :code:`rd_dynalloc` field
 static inline bool scc_ringdeque_is_allocd(void const *deque) {
     return ((unsigned char const *)deque)[-1];
+}
+
+//? .. c:function:: size_t scc_ringdeque_bytesize(size_t capacity, size_t elemsize, size_t npad)
+//?
+//?     Calculate the size a ringdeque with the given capacity, elemenet size
+//?     and number of paddign bytes would have
+//?
+//?     .. note::
+//?         Internal use only
+//?
+//?     :param capacity: The would-be capacity of the ringdeque
+//?     :param elemsize: The would-be element size of the ringdeque
+//?     :param npad: The would-be number of padding bytes between :ref:`rd_dynalloc <unsigned_char_rd_dynalloc>`
+//?                  and :ref:`rd_data <type_rd_data>`
+static inline size_t scc_ringdeque_bytesize(size_t capacity, size_t elemsize, size_t npad) {
+    return capacity * elemsize + sizeof(struct scc_ringdeque_base) + npad;
+}
+
+//? .. c:function:: struct scc_ringdeque_base *scc_ringdeque_alloc(size_t capacity, size_t size, size_t elemsize, size_t npad)
+//?
+//?     Allocate and partially initialize a :ref:`struct scc_ringdeque_base <scc_ringdeque_base>`
+//?
+//?     .. note::
+//?
+//?         Internal use only
+//?
+//?     :param capacity: The capacity of the ringdeque to be allocated
+//?     :param size: The numbes of elements to be written to the newly allocated ringdeque
+//?     :param elemsize: The size of each element in the ringdeque
+//?     :param npad: The number of padding bytes between :ref:`rd_dynalloc <unsigned_char_rd_dynalloc>`
+//?                  and :ref:`rd_data <type_rd_data>`.
+//?     :returns: Address of an newly allocated :ref:`struct scc_ringdeque_base <scc_ringdeque_base>`
+//?     :retval NULL: On memory allocation failure
+//?     :retval Valid address: On successfull allocation
+static struct scc_ringdeque_base *scc_ringdeque_alloc(size_t capacity, size_t size, size_t elemsize, size_t npad) {
+    size_t const nbytes = scc_ringdeque_bytesize(capacity, elemsize, npad);
+    struct scc_ringdeque_base *base = malloc(nbytes);
+    if(!base) {
+        return 0;
+    }
+    base->rd_size = size;
+    base->rd_capacity = capacity;
+    base->rd_buffer[npad - 2u] = npad - 2 * sizeof(unsigned char);
+    base->rd_buffer[npad - 1u] = 1;
+    return base;
+}
+
+//? .. c:function:: bool scc_ringdeque_grow(void **dequeaddr, size_t newcap, size_t elemsize)
+//?
+//?     Reallocate the ringdeque with increased capacity. The elements are repositioned
+//?     such that :ref:`rd_begin <size_t_rd_begin>` is 0
+//?
+//?     .. note::
+//?         Internal use only
+//?
+//?     :param dequeaddr: Address of the handle to the original ringdeque
+//?     :param newcap: The capacity of the ringdeque to be allocated
+//?     :param elemsize: Size of each element stored in the ringdeque
+//?     :returns: A :code:`_Bool` indicating whether the ringdeque could be reallocated
+//?     :retval NULL: Memory allocation failure
+//?     :retval Valid address: The ringdeque was successfully reallocated, :c:texpr:`*(void **)dequeaddr`
+//?                            is updated accordingly
+static bool scc_ringdeque_grow(void **dequeaddr, size_t newcap, size_t elemsize) {
+    size_t const npad = scc_ringdeque_impl_npad(*dequeaddr);
+    struct scc_ringdeque_base *prev = scc_ringdeque_impl_base(*dequeaddr);
+    struct scc_ringdeque_base *base = scc_ringdeque_alloc(newcap, prev->rd_size, elemsize, npad);
+    if(!base) {
+        return false;
+    }
+
+    assert(prev->rd_begin == prev->rd_end);
+    unsigned char *data = base->rd_buffer + npad;
+    size_t befwrap = prev->rd_capacity - prev->rd_begin;
+    size_t first = befwrap * elemsize;
+    unsigned char *firstsrc = (unsigned char *)*dequeaddr + prev->rd_begin * elemsize;
+    memcpy(data, firstsrc, first);
+    if(prev->rd_begin) {
+        memcpy(data + first, *dequeaddr, (prev->rd_size - befwrap) * elemsize);
+    }
+    base->rd_begin = 0;
+    base->rd_end = base->rd_size;
+
+    *dequeaddr = data;
+    return true;
 }
 
 void *scc_ringdeque_impl_init(void *deque, size_t offset, size_t capacity) {
@@ -30,4 +120,13 @@ void scc_ringdeque_free(void *deque) {
     if(scc_ringdeque_is_allocd(deque)) {
         free(scc_ringdeque_impl_base(deque));
     }
+}
+
+bool scc_ringdeque_impl_prepare_push(void *dequeaddr, size_t elemsize) {
+    struct scc_ringdeque_base *base = scc_ringdeque_impl_base(*(void **)dequeaddr);
+    if(base->rd_size < base->rd_capacity) {
+        return true;
+    }
+    size_t newcap = base->rd_capacity << 1u;
+    return scc_ringdeque_grow(dequeaddr, newcap, elemsize);
 }
