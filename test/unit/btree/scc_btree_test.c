17d3ff1e08d63146e77fe48046533794bb57733a
Vilhelm <vilhelm.engstrom@tuta.io>
2022-05-14 18:50:53 +0200

    btree: Add test for explicit order initialization

--- test/unit/btree/scc_btree_test.c
+++ test/unit/btree/scc_btree_test.c
@@ -10,3 +10,13 @@ void test_scc_btree_init(void) {
     scc_btree(int) btree = scc_btree_init(int, compare);
     scc_btree_free(btree);
 }
+
+void test_scc_btree_init_with_order(void) {
+    scc_btree(int) btree = scc_btree_init_with_order(int, compare, 2);
+    TEST_ASSERT_EQUAL_UINT64(2ull, scc_btree_order(btree));
+    scc_btree_free(btree);
+
+    btree = scc_btree_init_with_order(int, compare, 32);
+    TEST_ASSERT_EQUAL_UINT64(32ull, scc_btree_order(btree));
+    scc_btree_free(btree);
+}
