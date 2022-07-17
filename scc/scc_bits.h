5834f286be5252b4e923441b543449c688dc0bf8
Vilhelm <vilhelm.engstrom@tuta.io>
2022-07-17 13:49:31 +0200

    bits: Add macro determining if value is even

--- scc/scc_bits.h
+++ scc/scc_bits.h
@@ -4,4 +4,7 @@
 #define scc_bits_is_power_of_2(val)     \
     ((val) && (((val) & ~((val) - 1)) == (val)))
 
+#define scc_bits_is_even(val)           \
+    (!((val) & 0x01))
+
 #endif /* SCC_BITS_H */
