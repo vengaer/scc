4869cd29e3d92a1e69a5d70a25c1dd919bb5fb54
Vilhelm <vilhelm.engstrom@tuta.io>
2022-08-02 22:56:10 +0200

    algo: Fix lower bound with deuplicates in input

--- src/scc_algorithm.c
+++ src/scc_algorithm.c
@@ -8,14 +8,11 @@ size_t scc_algo_lower_bound(void const *key, void const *base, size_t nmemb, siz
     while(begin != end) {
         middle = begin + ((end - begin) >> 1u);
         cmp = compare(key, (unsigned char const *)base + middle * size);
-        if(cmp < 0) {
+        if(cmp <= 0) {
             end = middle;
         }
         else if(cmp > 0) {
             begin = middle + 1u;
-        }
-        else {
-            return middle;
         }
     }
 
