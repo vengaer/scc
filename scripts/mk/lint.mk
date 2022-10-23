026fd87b8c663319523782bc3471487469595af6
Vilhelm <vilhelm.engstrom@tuta.io>
2022-10-23 13:33:30 +0200

    lint: Run memory compatibility checker

--- scripts/mk/lint.mk
+++ scripts/mk/lint.mk
@@ -1,16 +1,21 @@
 ifndef __Lint_mk
 __Lint_mk := -
 
-__lint_py  := $(patsubst $(pyscripts)/%,$(builddir)/.lint.%.stamp,$(wildcard $(pyscripts)/*.$(pyext)))
+include $(mkscripts)/ext.mk
 
-tidyenable := clang-analyzer misc performance portability cert readability
-tidyignore := clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling \
-              readability-function-cognitive-complexity                            \
-              readability-uppercase-literal-suffix readability-magic-numbers       \
-              cert-dcl16-c readability-identifier-length
+__lint_py    := $(patsubst $(pyscripts)/%,$(builddir)/.lint.%.stamp,$(wildcard $(pyscripts)/*.$(pyext)))
 
-__comma    :=,
-tidychecks := $(subst $(subst ,, ),$(__comma),$(addsuffix -*,$(tidyenable)) $(addprefix -,$(tidyignore)))
+tidyenable   := clang-analyzer misc performance portability cert readability
+tidyignore   := clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling \
+                readability-function-cognitive-complexity                            \
+                readability-uppercase-literal-suffix readability-magic-numbers       \
+                cert-dcl16-c readability-identifier-length
+
+__comma      :=,
+tidychecks   := $(subst $(subst ,, ),$(__comma),$(addsuffix -*,$(tidyenable)) $(addprefix -,$(tidyignore)))
+
+__mcompat    := $(pyscripts)/mcompatchk.$(pyext)
+__mcompatcfg := $(root)/.mcompat.$(jsonext)
 
 $(__lint_py): $$(patsubst $(builddir)/.lint.%.stamp,$(pyscripts)/%,$$@) $(__all_mkfiles) | $(builddir)
 	$(call echo-lint,$(notdir $<))
@@ -30,7 +35,16 @@ $(eval
 	            $(TIDY) $(TIDYFLAGS) $$< -- $(CPPFLAGS) $(CFLAGS)
 	            $(TOUCH) $$@
 
-            lint: $(__lint_$(__s)))))
+            lint: $(__lint_$(__s))))
+    $(foreach __s,$(wildcard $(__node_path)/*.$(hext)),
+        $(eval __compatchk_$(__s) := $(__node_builddir)/.mcompatchk.$(notdir $(__s)).stamp)
+
+        $(__compatchk_$(__s)): $(__s) $(__config_header) $(__mcompatcfg) $(__all_mkfiles) | $(__node_builddir) $(submodules)
+	        $$(call echo-mcompat,$$(notdir $$<))
+	        $(PYTHON) $(__mcompat) -c $(__mcompatcfg) -I$(root)/submodules/pycparser/utils/fake_libc_include $$<
+	        $(TOUCH) $$@
+
+        lint: $(__compatchk_$(__s))))
 endef
 
 endif
