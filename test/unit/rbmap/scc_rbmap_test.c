ad7d119be78028f0d15037a37ad6b6a2d4353397
Vilhelm <vilhelm.engstrom@tuta.io>
2022-10-29 13:41:17 +0200

    rbtree: Add insertion and find tests

---
+++ test/unit/rbmap/scc_rbmap_test.c
@@ -0,0 +1,47 @@
+#include <inspect/scc_rbtree_inspect.h>
+#include <scc/scc_mem.h>
+#include <scc/scc_rbmap.h>
+
+#include <unity.h>
+
+static int compare(void const *left, void const *right) {
+    return *(int const *)left - *(int const *)right;
+}
+
+void test_scc_rbmap_insert(void) {
+    enum { TEST_SIZE = 2400 };
+
+    scc_inspect_mask mask;
+    scc_rbmap(int, int) rbmap = scc_rbmap_new(int, int, compare);
+    for(int i = 0; i < TEST_SIZE; i++) {
+        TEST_ASSERT_TRUE(scc_rbmap_insert(&rbmap, i, i));
+        TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_rbmap_size(rbmap));
+        for(int j = 0; j <= i; j++) {
+            TEST_ASSERT_FALSE(scc_rbmap_insert(&rbmap, j, j));
+        }
+        mask = scc_rbtree_inspect_properties(rbmap);
+        TEST_ASSERT_EQUAL_UINT64(mask & SCC_RBTREE_ERR_MASK, 0ull);
+    }
+    scc_rbmap_free(rbmap);
+}
+
+void test_scc_rbmap_find(void) {
+    enum { TEST_SIZE = 1200 };
+    scc_rbmap(int, int) rbmap = scc_rbmap_new(int, int, compare);
+
+    int *val;
+    for(int i = 0; i < TEST_SIZE; ++i) {
+        TEST_ASSERT_TRUE(scc_rbmap_insert(&rbmap, i, i));
+        val = scc_rbmap_find(rbmap, i);
+        TEST_ASSERT_TRUE(!!val);
+        TEST_ASSERT_EQUAL_INT32(i, *val);
+        *val = i << 1;
+
+        for(int j = 0; j < i; ++j) {
+            val = scc_rbmap_find(rbmap, j);
+            TEST_ASSERT_EQUAL_INT32(2 * j, *val);
+        }
+    }
+    scc_rbmap_free(rbmap);
+}
+
