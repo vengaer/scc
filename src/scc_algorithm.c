18da2c997250dfc1c3a58c5df7e6aba0447debdc
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-11-19 21:57:30 +0100

    algo: Improve lower_bound cache efficiency for smaller workloads
    Exact cut-off will have to be determined via benchmarking

--- src/scc_algorithm.c
+++ src/scc_algorithm.c
@@ -5,6 +5,8 @@
 #define SIZE_MASK ((~((size_t)0u)) >> 1u)
 #define SIZE_SHIFT ((sizeof(size_t) * CHAR_BIT) - 1u)
 
+#define LOWER_BOUND_LINEAR_LIM 20u
+
 size_t scc_algo_lower_bound(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *)) {
     return scc_algo_lower_bound_eq(key, base, nmemb, size, compare) & SIZE_MASK;
 }
@@ -14,14 +16,24 @@ size_t scc_algo_lower_bound_eq(void const *key, void const *base, size_t nmemb, 
     size_t end = nmemb;
     size_t middle;
     int cmp = 0;
-    while(begin != end) {
-        middle = begin + ((end - begin) >> 1u);
-        cmp = compare(key, (unsigned char const *)base + middle * size);
-        if(cmp <= 0) {
-            end = middle;
+    if(nmemb < LOWER_BOUND_LINEAR_LIM) {
+        for(; begin < nmemb; ++begin) {
+            cmp = compare(key, (unsigned char const *)base + begin * size);
+            if(cmp <= 0) {
+                break;
+            }
         }
-        else if(cmp > 0) {
-            begin = middle + 1u;
+    }
+    else {
+        while(begin != end) {
+            middle = begin + ((end - begin) >> 1u);
+            cmp = compare(key, (unsigned char const *)base + middle * size);
+            if(cmp <= 0) {
+                end = middle;
+            }
+            else if(cmp > 0) {
+                begin = middle + 1u;
+            }
         }
     }
 
