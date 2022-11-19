ad9a560a2142c391f11ddd86cfe2cfbabc7fb9de
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-11-19 15:13:55 +0100

    btmap: Add test verifying even-order creation

--- test/unit/btmap/scc_btmap_even_order_test.c
+++ test/unit/btmap/scc_btmap_even_order_test.c
@@ -12,3 +12,13 @@ void test_scc_btmap_new(void) {
     scc_btmap(int, int) btmap = scc_btmap_new(int, int, compare);
     scc_btmap_free(btmap);
 }
+
+void test_scc_btmap_with_order(void) {
+    scc_btmap(int, int) btmap = scc_btmap_with_order(int, int, compare, 4);
+    TEST_ASSERT_EQUAL_UINT64(4ull, scc_btmap_order(btmap));
+    scc_btmap_free(btmap);
+
+    btmap = scc_btmap_with_order(int, int, compare, 32);
+    TEST_ASSERT_EQUAL_UINT64(32ull, scc_btmap_order(btmap));
+    scc_btmap_free(btmap);
+}
