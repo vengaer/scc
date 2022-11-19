2bb0789f5d8496ac6a48f38562585b18ad90bb8f
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-11-19 19:25:26 +0100

    algo: Add lower bound version that signals equality
    The _eq versions sets the MSB in the return value iff the
    element at the computed index compares equal to the supplied key

--- scc/scc_algorithm.h
+++ scc/scc_algorithm.h
@@ -43,4 +43,21 @@
 //?         assert(scc_lower_bound(&(int){ 0 }, seq2, 3, sizeof(int), compare) == 0);
 size_t scc_algo_lower_bound(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *));
 
+//? .. _scc_algo_lower_bound_eq:
+//? .. c:function:: size_t scc_algo_lower_bound_eq(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *))
+//?
+//?     Lite :ref:`scc_algo_lower_bound <scc_algo_lower_bound>` but sets the MSB in the return value
+//?     if the value at the found index equals that of the :code:`key`.
+//?
+//?     :param key: The key relative which the lower bound is to be found
+//?     :param base: Base address of the consecutive sequence to search
+//?     :param nmemb: Number of elements in the consequtive sequence starting at :code:`base`
+//?     :param size: The size of each element in the sequence
+//?     :param compare: The comparison function to use. It should accept the :code:`key` as its
+//?                     first parameter and return an :c:expr:`int` less than, equal to, or greater
+//?                     than 0 if the :code:`key` is found to be less than, equal to or greater than the array member, respectively.
+//?                     See the `bsearch man page <https://man7.org/linux/man-pages/man3/bsearch.3.html>` for more information.
+//?     :returns: Index of the first element greater than or equal to :code:`key` with the MSB set if the element is equal to the key
+size_t scc_algo_lower_bound_eq(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *));
+
 #endif /* SCC_ALGORITHM_H */
--- src/scc_algorithm.c
+++ src/scc_algorithm.c
@@ -1,10 +1,19 @@
 #include <scc/scc_algorithm.h>
 
+#include <limits.h>
+
+#define SIZE_MASK ((~((size_t)0u)) >> 1u)
+#define SIZE_SHIFT ((sizeof(size_t) * CHAR_BIT) - 1u)
+
 size_t scc_algo_lower_bound(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *)) {
+    return scc_algo_lower_bound_eq(key, base, nmemb, size, compare) & SIZE_MASK;
+}
+
+size_t scc_algo_lower_bound_eq(void const *key, void const *base, size_t nmemb, size_t size, int(*compare)(void const *, void const *)) {
     size_t begin = 0u;
     size_t end = nmemb;
     size_t middle;
-    int cmp;
+    int cmp = 0;
     while(begin != end) {
         middle = begin + ((end - begin) >> 1u);
         cmp = compare(key, (unsigned char const *)base + middle * size);
@@ -16,5 +25,5 @@ size_t scc_algo_lower_bound(void const *key, void const *base, size_t nmemb, siz
         }
     }
 
-    return begin;
+    return begin | (((size_t)!cmp) << SIZE_SHIFT);
 }
