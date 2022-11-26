53f001e222a4f3514863267fa3dbb45d22028f91
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-11-26 17:01:05 +0100

    btmap: Add test for insertion with default order

--- test/unit/btmap/scc_btmap_even_order_test.c
+++ test/unit/btmap/scc_btmap_even_order_test.c
@@ -1,6 +1,8 @@
-#include <inspect/scc_btree_inspect.h>
+#include <inspect/scc_btmap_inspect.h>
 #include <scc/scc_btmap.h>
 #include <scc/scc_mem.h>
+
+#include <string.h>
 
 #include <unity.h>
 
@@ -40,5 +42,31 @@ void test_scc_btmap_insert_replace(void) {
 void test_scc_btmap_size_empty(void) {
     scc_btmap(int, int) btmap = scc_btmap_new(int, int, compare);
     TEST_ASSERT_EQUAL_UINT64(0ull, scc_btmap_size(btmap));
+    scc_btmap_free(btmap);
+}
+
+void test_scc_btmap_insert_default_order(void) {
+    enum { TEST_SIZE = 3200 };
+    typedef struct { char p[32]; } str;
+    int r;
+    str s;
+    str *sp;
+    scc_btmap(int, str) btmap = scc_btmap_new(int, str, compare);
+    for(int i = 0; i < TEST_SIZE; ++i) {
+        r = snprintf(s.p, sizeof(s.p), "s%d", i);
+        TEST_ASSERT_TRUE(r >= 0 && r < (int)sizeof(s.p));
+        TEST_ASSERT_TRUE(scc_btmap_insert(&btmap, i, s));
+        TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_btmap_size(btmap));
+        TEST_ASSERT_EQUAL_UINT32(0u, scc_btmap_inspect_invariants(btmap));
+
+        for(int j = 0; j < i; ++j) {
+            sp = scc_btmap_find(btmap, j);
+            TEST_ASSERT_TRUE(sp);
+            r = snprintf(s.p, sizeof(s.p), "s%d", j);
+            TEST_ASSERT_TRUE(r >= 0 && r < (int)sizeof(s.p));
+            TEST_ASSERT_EQUAL_INT32(0, strcmp(sp->p, s.p));
+        }
+    }
+
     scc_btmap_free(btmap);
 }
