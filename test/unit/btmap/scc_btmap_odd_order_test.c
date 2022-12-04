37b5e060c24d8969e1393e1897b4c00e38a6a14b
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2022-12-04 15:18:39 +0100

    btmap: Add test for odd-order instertion forcing middle split

--- test/unit/btmap/scc_btmap_odd_order_test.c
+++ test/unit/btmap/scc_btmap_odd_order_test.c
@@ -49,3 +49,40 @@ void test_scc_btmap_insert_odd_order_reverse(void) {
 
     scc_btmap_free(btmap);
 }
+
+void test_scc_btmap_insert_odd_order_middle_split(void) {
+    scc_btmap(int, int) btmap = scc_btmap_with_order(int, int, compare, 5);
+
+    int *p;
+    for(int i = 0; i < 15; ++i) {
+        TEST_ASSERT_TRUE(scc_btmap_insert(&btmap, i, i + 1));
+        for(int j = 0; j < i; ++j) {
+            p = scc_btmap_find(btmap, j);
+            TEST_ASSERT_TRUE(p);
+            TEST_ASSERT_EQUAL_INT32(j + 1, *p);
+        }
+    }
+    TEST_ASSERT_TRUE(scc_btmap_insert(&btmap, 16, 17));
+    TEST_ASSERT_TRUE(scc_btmap_insert(&btmap, 17, 18));
+    for(int j = 0; j < 15; ++j) {
+        p = scc_btmap_find(btmap, j);
+        TEST_ASSERT_TRUE(p);
+        TEST_ASSERT_EQUAL_INT32(j + 1, *p);
+    }
+
+    p = scc_btmap_find(btmap, 16);
+    TEST_ASSERT_TRUE(p);
+    TEST_ASSERT_EQUAL_INT32(17, *p);
+    p = scc_btmap_find(btmap, 17);
+    TEST_ASSERT_TRUE(p);
+    TEST_ASSERT_EQUAL_INT32(18, *p);
+
+    TEST_ASSERT_EQUAL_UINT32(0u, scc_btmap_inspect_invariants(btmap));
+
+    TEST_ASSERT_TRUE(scc_btmap_insert(&btmap, 15, 0));
+    p = scc_btmap_find(btmap, 15);
+    TEST_ASSERT_TRUE(p);
+    TEST_ASSERT_EQUAL_INT32(0, *p);
+    TEST_ASSERT_EQUAL_UINT32(0u, scc_btmap_inspect_invariants(btmap));
+    scc_btmap_free(btmap);
+}
