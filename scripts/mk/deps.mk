d5788199ad51dee0334ceab85b665595b08af50d
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-06 11:05:30 +0100

    simdemu: Separate hashtab and map tests for improved deps handling

--- scripts/mk/deps.mk
+++ scripts/mk/deps.mk
@@ -1,10 +1,12 @@
 ifndef __Deps_mk
 __Deps_mk := _
 
-btmap_deps  := algorithm arena svec
-btree_deps  := algorithm arena svec vec
-rbmap_deps  := arena rbtree
-rbtree_deps := arena
-stack_deps  := svec
+btmap_deps           := algorithm arena svec
+btree_deps           := algorithm arena svec vec
+rbmap_deps           := arena rbtree
+rbtree_deps          := arena
+stack_deps           := svec
+hashmap_simdemu_deps := hashmap
+hashtab_simdemu_deps := hashtab
 
 endif # __Deps_mk
