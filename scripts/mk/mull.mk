fbdc38c4bcb198e460c46195c52b339441ce5d70
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-06 14:22:36 +0100

    build: Provide mull with library search paths

--- scripts/mk/mull.mk
+++ scripts/mk/mull.mk
@@ -4,6 +4,8 @@ __Mull_mk := _
 __mull_cflags  = -fexperimental-new-pass-manager -fpass-plugin=$(mull_ir_frontend) -g -grecord-command-line
 __mull_ldflags = -fexperimental-new-pass-manager -fpass-plugin=$(mull_ir_frontend)
 
-MULLFLAGS      = --no-test-output --workers=$(cpu_cores)
+__ldpaths     := $(addprefix --ld-search-path=,$(wildcard $(addsuffix lib,/ /usr/ /usr/local/)))
+
+MULLFLAGS      = --no-test-output --workers=$(cpu_cores) $(__ldpaths)
 
 endif
