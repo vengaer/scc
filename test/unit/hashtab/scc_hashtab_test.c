00b264a4443d3a516137157705b4f3e049e88c41
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-07 16:08:29 +0100

    hashtab: Add test for fnv1a implementation
    References from https://md5calc.com/hash/fnv1a64

--- test/unit/hashtab/scc_hashtab_test.c
+++ test/unit/hashtab/scc_hashtab_test.c
@@ -1,7 +1,9 @@
 #include <inspect/hashtab_inspect.h>
+#include <scc/bug.h>
 #include <scc/hashtab.h>
 #include <scc/mem.h>
 
+#include <limits.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -310,4 +312,30 @@ void test_scc_hashtab_metadata_address_on_rehash(void) {
 
     TEST_ASSERT_EQUAL_PTR(md, scc_hashtab_inspect_metadata(tab));
     scc_hashtab_free(tab);
+}
+
+void test_scc_hashtab_fnv1a64(void) {
+    static char const *strings[] = {
+        "permian",
+        "palaeocene",
+        "eocene",
+        "oligocene"
+    };
+
+    static unsigned long long hashes[] = {
+        0x88224508f1e2e011ull,
+        0x7b96bcef58fde340ull,
+        0xfc0e1183fbd62254ull,
+        0x83b3ee57387597f2ull
+    };
+
+    scc_static_assert(ULLONG_MAX >= 0xffffffffffffffffull,
+                        "Non-confromant implementation (n2310 5.2.4.2.1)");
+
+    unsigned long long hash;
+    scc_static_assert(scc_arrsize(strings) == scc_arrsize(hashes));
+    for(unsigned i = 0u; i < scc_arrsize(hashes); ++i) {
+        hash = scc_hashtab_fnv1a(strings[i], strlen(strings[i]));
+        TEST_ASSERT_EQUAL_UINT64(hashes[i], hash);
+    }
 }
