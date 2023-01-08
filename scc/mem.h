140b17aabcbdb4beba324e22702623db31aa8300
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-08 11:59:40 +0100

    mem: Add memcpy/memmove wrapper optionally validating sizes

--- scc/mem.h
+++ scc/mem.h
@@ -1,6 +1,7 @@
 #ifndef SCC_MEM_H
 #define SCC_MEM_H
 
+#include <assert.h>
 #include <stddef.h>
 
 #define scc_container_qual(addr, type, member, qual)    \
@@ -17,5 +18,24 @@
 
 #define scc_align(addr, bound)                          \
     (((addr) + (bound) - 1u) & ~((bound) - 1u))
+
+#ifdef SCC_INTERCEPT_NULLSIZE_COPIES
+
+#ifdef NDEBUG
+#error INTERCEPT_NULLSIZE_COPIES has no effect with NDEBUG defined
+#endif
+
+#define scc_memmove(dst, src, n)                        \
+    (assert((n) > 0u), memmove((dst), (src), (n)))
+
+#define scc_memcpy(dst, src, n)                         \
+    (assert((n) > 0u), memcpy((dst), (src), (n)))
+
+#else
+
+#define scc_memmove(...) memmove(__VA_ARGS__)
+#define scc_memcpy(...) memcpy(__VA_ARGS__)
+
+#endif
 
 #endif /* SCC_MEM_H */
