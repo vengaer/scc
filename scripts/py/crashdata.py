88059ff1ffbd8dc0f32b7569f027fc8a7df2f871
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-01-21 18:15:55 +0100

    crashdata: Remove skip parameter

--- scripts/py/crashdata.py
+++ scripts/py/crashdata.py
@@ -17,22 +17,19 @@ _TYPES = {
     8: 'uint64_t'
 }
 
-def read_and_dump(file, outfile, skip, group):
+def read_and_dump(file, outfile, group):
     ''' Read the file and dump its contents '''
-    with open(file, 'rb') as handle:
-        data = handle.read()[skip:]
-
     fmt = f'0x{{:0{2 * int(group)}x}}'
     with FileWrHandle(outfile) if outfile is not None else StdoutWrHandle() as handle:
         handle.write(f'{_TYPES[int(group)]} data[] = {{\n{4 * " "}')
         with open(file, 'rb') as infile:
             read_chunk = functools.partial(infile.read, int(group))
-            sz = os.fstat(infile.fileno()).st_size
+            size = os.fstat(infile.fileno()).st_size
 
             for i, chunk in enumerate(iter(read_chunk, b'')):
                 val = int.from_bytes(chunk, sys.byteorder)
                 handle.write(fmt.format(val))
-                if i < sz // int(group):
+                if i < size // int(group):
                     handle.write(',')
                 handle.write(' ')
                 if not (i + 1) % _LINE_LIM:
@@ -44,7 +41,5 @@ if __name__ == '__main__':
     parser.add_argument('file', metavar='FILE', type=str, help='Path to the crash file')
     parser.add_argument('-o', '--outfile', action='store', default=None,
                         help='Path to write the generated array to')
-    parser.add_argument('-s', '--skip', action='store', type=int, default=0,
-                        help='Number of initial bytes to discard')
     parser.add_argument('-g', '--group', default=1, help='Size of each input variable')
     read_and_dump(**vars(parser.parse_args()))
