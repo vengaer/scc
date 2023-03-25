2f5ed7589a9da8d766ba040f84b8090d9dd3edbd
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2023-03-25 15:27:56 +0100

    stack: Remove test requiring scc_vec

--- test/unit/stack/scc_stack_test.c
+++ test/unit/stack/scc_stack_test.c
@@ -89,28 +89,3 @@ void test_scc_stack_empty(void) {
     TEST_ASSERT_TRUE(scc_stack_empty(stack));
     scc_stack_free(stack);
 }
-
-void test_scc_stack_custom_container(void) {
-    enum { TEST_SIZE = 100 };
-    scc_stack(int) default_container = scc_stack_new(int);
-    TEST_ASSERT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY + 0ull, scc_stack_capacity(default_container));
-    scc_stack_free(default_container);
-
-#undef SCC_STACK_CONTAINER
-    /* Use scc_vec instead of scc_svec */
-#define SCC_STACK_CONTAINER scc_vec
-
-    scc_stack(int) custom_container = scc_stack_new(int);
-    TEST_ASSERT_NOT_EQUAL_UINT64(SCC_SVEC_STATIC_CAPACITY + 0ull, scc_stack_capacity(custom_container));
-    for(int i = 0; i < TEST_SIZE; ++i) {
-        TEST_ASSERT_TRUE(scc_stack_push(&custom_container ,i));
-        TEST_ASSERT_EQUAL_INT32(i, scc_stack_top(custom_container));
-        TEST_ASSERT_EQUAL_UINT64(i + 0ull, scc_stack_size(custom_container) - 1ull);
-    }
-    for(int i = TEST_SIZE; i > 0; --i) {
-        TEST_ASSERT_EQUAL_INT32(i - 1, scc_stack_top(custom_container));
-        TEST_ASSERT_EQUAL_UINT64(i + 0ull, scc_stack_size(custom_container));
-        scc_stack_pop(custom_container);
-    }
-    scc_stack_free(custom_container);
-}
