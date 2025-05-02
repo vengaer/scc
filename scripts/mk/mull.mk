f754b6125cc10b52679df9d69a6b70ea1848c30b
Vilhelm <vilhelm.engstrom@tuta.io>
2025-05-03 01:00:56 +0200

    build: Remove -fexperimental-new-pass-manager flag
    This has been a NOP in upstream LLVM for a while, and passing it is even
    an error starting in llvm 19

--- scripts/mk/mull.mk
+++ scripts/mk/mull.mk
@@ -1,8 +1,8 @@
 ifndef __Mull_mk
 __Mull_mk := _
 
-__mull_cflags  = -fexperimental-new-pass-manager -fpass-plugin=$(mull_ir_frontend) -g -grecord-command-line
-__mull_ldflags = -fexperimental-new-pass-manager -fpass-plugin=$(mull_ir_frontend)
+__mull_cflags  = -fpass-plugin=$(mull_ir_frontend) -g -grecord-command-line
+__mull_ldflags = -fpass-plugin=$(mull_ir_frontend)
 
 __ldpaths     := $(addprefix --ld-search-path=,$(wildcard $(addsuffix lib,/ /usr/ /usr/local/)))
 
