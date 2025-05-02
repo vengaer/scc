fc48d73b2d3e42f49f97619b5002428df90a3c1e
Vilhelm <vilhelm.engstrom@tuta.io>
2025-05-02 22:00:06 +0200

    cmake: Use python3 explicitly

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -4,28 +4,28 @@ set (CONFOPTS -g SCC_CONFIG_H -c ${CMAKE_CURRENT_BINARY_DIR}/.scconfig.cmake.cac
 set (CONFHDR ${CMAKE_CURRENT_SOURCE_DIR}/scc/config.h)
 
 add_custom_target (.config.init.cmake.stamp
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} -o ${CONFHDR} init
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} -o ${CONFHDR} init
 )
 
 add_custom_target (.config.version.cmake.stamp
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_MAJOR ${SCC_VERSION_MAJOR} -C "Library major version"
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_MINOR ${SCC_VERSION_MINOR} -C "Library minor version"
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_PATCH ${SCC_VERSION_PATCH} -C "Library patch version"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_MAJOR ${SCC_VERSION_MAJOR} -C "Library major version"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_MINOR ${SCC_VERSION_MINOR} -C "Library minor version"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_VERSION_PATCH ${SCC_VERSION_PATCH} -C "Library patch version"
     DEPENDS .config.init.cmake.stamp
 )
 
 add_custom_target(.config.bitarch.cmake.stamp
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_BITARCH_$<IF:$<STREQUAL:${CMAKE_SIZEOF_VOID_P},8>,64,32> -C "System architecture"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_BITARCH_$<IF:$<STREQUAL:${CMAKE_SIZEOF_VOID_P},8>,64,32> -C "System architecture"
     DEPENDS .config.version.cmake.stamp
 )
 
 add_custom_target (.config.simd.cmake.stamp
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_SIMD_ISA ${SCC_SIMD_ISA} -C "SIMD instruction set architecture"
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_HWVEC_SIZE ${SCC_VECSIZE} -C "Size of hardware SIMD vectors"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_SIMD_ISA ${SCC_SIMD_ISA} -C "SIMD instruction set architecture"
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} add SCC_HWVEC_SIZE ${SCC_VECSIZE} -C "Size of hardware SIMD vectors"
     DEPENDS .config.bitarch.cmake.stamp
 )
 
 add_custom_target (config
-    COMMAND ${Python_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} commit
+    COMMAND ${Python3_EXECUTABLE} ${SCCONFIG} ${CONFOPTS} commit
     DEPENDS .config.$<IF:$<BOOL:${SCC_SIMD_SUPPORTED}>,simd,bitarch>.cmake.stamp
 )
