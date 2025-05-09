e5814fd18646fa046b98772471738dd777fe5579
Vilhelm <vilhelm.engstrom@tuta.io>
2025-05-09 23:39:10 +0200

    hashtab: Add fuzz failure for killing remaining mutants

--- test/unit/swar/hashtab_swar/scc_swar_hashtab_fuzzer_failures.c
+++ test/unit/swar/hashtab_swar/scc_swar_hashtab_fuzzer_failures.c
@@ -27,6 +27,7 @@ static void run_fuzzer_test(uint32_t const *data, size_t n) {
 
     uint32_t const *elem;
     for(unsigned i = 0u; i < n; ++i) {
+        printf("%u\n", i);
         TEST_ASSERT_TRUE(scc_hashtab_insert(&tab, data[i]));
         TEST_ASSERT_EQUAL_UINT64(i + 1ull, scc_hashtab_size(tab));
 
@@ -69,6 +70,23 @@ void test_scc_swar_hashtab_fuzzer_failure0(void) {
         0xd03bd00d, 0xafaf0d0d, 0xc48001af, 0x12ff2c8,
         0xd373b80,  0xd0d0d0d0, 0x3afafafa, 0x7f000060,
         0xc8c48000, 0x2f2f2ff2, 0x1d0d031,  0x43432a80
+    };
+
+    run_fuzzer_test(vals, scc_arrsize(vals));
+}
+
+void test_scc_swar_hashtab_fuzzer_failure1(void) {
+    static uint32_t vals[] = {
+        0xff0aff0a, 0x8e0382ff, 0x2dc0fd72, 0xffffff2e,
+        0xffffffff, 0xb198ffff, 0x32360e0e, 0xb1000000,
+        0xb1b1b1b1, 0xe3e3e3b1, 0x000000e3, 0x60000000,
+        0xffe3e3e3, 0x0e0ee1ff, 0x290a0a0e, 0x0e0eb129,
+        0x999999b1, 0x99993f99, 0x9999999d, 0x99996367,
+        0xffffff99, 0xfafafa04, 0xff2fffff, 0x002dc0fd,
+        0x82ff606d, 0xfd728e03, 0x002e2dc0, 0x0097606d,
+        0x0099fa00, 0xfafafafa, 0xdbdb3afa, 0x990dcadb,
+        0xdd999999, 0xe1dddddd, 0xe1e1e1e1, 0xdddddddd,
+        0xfc7effdd, 0xec8ef5c6, 0xd0d0d00e, 0x2a8001d0
     };
 
     run_fuzzer_test(vals, scc_arrsize(vals));
