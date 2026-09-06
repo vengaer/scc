f51217132e3a26c2582927c620d6102415e58292
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-06 15:21:23 +0200

    rbtree: Remove breathe docs
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- scc/rbtree.h
+++ scc/rbtree.h
@@ -7,56 +7,10 @@
 
 #include <stddef.h>
 
-/**
- * Expands to a type suitable for referring to an ``rbmap`` mapping \a keytype to \a valuetype
- *
- * \param type The type to store in the red-black tree
- */
 #define scc_rbtree(type) type *
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbtree_iter:
- * \endverbatim
- *
- * Expands to a type suitable for iterating an ``rbtree`` containing
- * \a type instances
- *
- * \param type The type stored in the ``rbtree``
- */
 #define scc_rbtree_iter(type) type const *
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbcompare:
- *  \endverbatim
- *
- * Signature of the function used for comparing keys in a ``rbtree``.
- *
- * <table>
- *  <caption id="rbcmp_rv">Return value</caption>
- *  <tr><th>Condition <th>Value
- *  <tr><td>left < right<td> < 0
- *  <tr><td>left == right<td> 0
- *  <tr><td>left > right<td> > 0
- * </table>
- *
- * \verbatim embed:rst:leading-asterisk
- *
- * .. literalinclude:: /../examples/btree/comparators.c
- *      :caption: Example ``int`` comparator.
- *      :start-at: compare_int
- *      :end-at: }
- *      :language: c
- *
- * .. literalinclude:: /../examples/btree/comparators.c
- *      :caption: Example ``string`` comparator.
- *      :start-at: compare_str
- *      :end-at: }
- *      :language: c
- *
- * \endverbatim
- */
 typedef int(*scc_rbcompare)(void const *, void const *);
 
 enum scc_rbcolor {
@@ -172,27 +126,6 @@ void *scc_rbtree_impl_new(struct scc_rbtree_base *base, size_t coff);
 
 void *scc_rbtree_impl_new_dyn(size_t treesz, struct scc_arena *arena, scc_rbcompare compare, size_t coff, size_t dataoff);
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbtree_new:
- * \endverbatim
- *
- * Create an ``rbtree`` instance storing elements of the provided \a type.
- *
- * The call cannot fail.
- *
- * The instance is allocated in the scope where the macro is invoked and should therefore
- * not be returned.
- *
- * \sa @verbatim embed:rst:inline :ref:`scc_rbtree_new_dyn <scc_rbtree_new_dyn>` @endverbatim
- *     for the counterpart relying on dynamic allocation.
- *
- * \param type Type of the elements to be stored in the ``rbtree``
- * \param compare Pointer to the comparison function to use. The signature should match
- *                @verbatim embed:rst:inline :ref:`scc_rbcompare <scc_rbcompare>` @endverbatim
- *
- * \return An opaque pointer referring to an ``rbtree`` allocated in the frame of the calling function
- */
 #define scc_rbtree_new(type, compare)                                                       \
     (type *)scc_rbtree_impl_new(                                                            \
         (void *)&(scc_rbtree_impl_layout(type)) {                                           \
@@ -205,23 +138,6 @@ void *scc_rbtree_impl_new_dyn(size_t treesz, struct scc_arena *arena, scc_rbcomp
         scc_rbtree_impl_curroff(type)                                                       \
    )
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbtree_new_dyn:
- * \endverbatim
- *
- * Like @verbatim embed:rst:inline :ref:`scc_rbtree_new <scc_rbtree_new>` @endverbatim except for
- * the ``rbtree`` being allocated on the heap rather than the stack.
- *
- * \note Unlike @verbatim embed:rst:inline :ref:`scc_rbtree_new <scc_rbtree_new>` @endverbatim,
- *       ``scc_rbtree_new_dyn`` may fail. If it does, ``NULL`` is returned.
- *
- * \param type Type of the elements stored in the ``rbtree``
- * \param compare Pointer to the comparison function to use. The signature should match
- *                @verbatim embed:rst:inline :ref:`scc_rmcompare <scc_rmcompare>` @endverbatim
- *
- * \return An opaque pointer referring to a dynamically allocated``rbtree``, or ``NULL`` on failure.
- */
 #define scc_rbtree_new_dyn(type, compare)                                                   \
     (type *)scc_rbtree_impl_new_dyn(                                                        \
         sizeof(scc_rbtree_impl_layout(type)),                                               \
@@ -271,48 +187,17 @@ inline size_t scc_rbnode_bkoff(void const *iter) {
 #define scc_rbtree_impl_base(rbtree)                                                        \
     scc_rbtree_impl_base_qual(rbtree,)
 
-/**
- * Query the size of the given ``rbtree``.
- *
- * \param rbtree Handle referring to the ``rbtree``
- *
- * \return Number of elements stored in the ``rbtree``
- */
 inline size_t scc_rbtree_size(void const *rbtree) {
     struct scc_rbtree_base const *base = scc_rbtree_impl_base_qual(rbtree, const);
     return base->rb_size;
 }
 
-/**
- * Determine whether or not the given ``rbtree`` is empty.
- *
- * \param rbtree Handle referring to the ``rbtree``
- *
- * \return ``true`` if the ``rbtree`` is empty, ``false`` if it's not
- */
 inline _Bool scc_rbtree_empty(void const *rbtree) {
     return !scc_rbtree_size(rbtree);
 }
 
-/**
- * Remove all elements from the given ``rbtree``.
- *
- * \param rbtree Handle identifying the ``rbtree`` to clear
- */
 void scc_rbtree_clear(void *rbtree);
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbtree_free:
- * \endverbatim
- *
- * Reclaim memory allocated for the provided ``rbtree``.
- *
- * The parameter must refer to a valid ``rbtree`` instantiated using on of
- * the ``rbtree`` @verbatim embed:rst:inline :ref:`initialization constructs <rbtree_init>` @endverbatim.
- *
- * \param rbtree Handle referring to the ``rbtree`` to free.
- */
 void scc_rbtree_free(void *rbtree);
 
 void *scc_rbtree_impl_generic_insert(void *rbtreeaddr, size_t elemsize);
@@ -321,58 +206,16 @@ inline _Bool scc_rbtree_impl_insert(void *rbtreeaddr, size_t elemsize) {
     return scc_rbtree_impl_generic_insert(rbtreeaddr, elemsize) == *(void **)rbtreeaddr;
 }
 
-/**
- * Insert the given element into the ``rbtree``.
- *
- * The \a value parameter must necessarily be the same type of that with which
- * the ``rbtree`` was instantiated. If it's not, it's subject to implicit conversion.
- *
- * \note ``scc_rbtree_insert`` takes a \b pointer to the handle returned by one of the
- * @verbatim embed:rst:inline :ref:`initialization constructs <rbtree_init>` @endverbatim,
- * \b not the handle itself.
- *
- * \param rbtreeaddr Address of the ``rbtree`` handle
- * \param value The element to insert
- *
- * \return ``true`` if the insertion was successful, otherwise ``false``.
- */
 #define scc_rbtree_insert(rbtreeaddr, value)                                                \
     scc_rbtree_impl_insert((**(rbtreeaddr) = (value), rbtreeaddr), sizeof(**(rbtreeaddr)))
 
 void const *scc_rbtree_impl_find(void const *rbtree);
 
-/**
- * Look up an element in the ``rbtree``.
- *
- * If the element is found, a ``const``-qualified pointer referring to the element in the tree is returned.
- *
- * The \a value parameter must not necessarily be the same type as the one
- * with which the ``rbtree`` was instantiated. If it is not, it is implicitly converted
- * to the type stored in the instance.
- *
- *
- * \param rbtree Handle identifying the ``rbtree``
- * \param value The element to search for
- *
- * \return A pointer to the element in the tree, or ``NULL`` if it's not found.
- */
 #define scc_rbtree_find(rbtree, value)                                                      \
     scc_rbtree_impl_find((*(rbtree) = (value), (rbtree)))
 
 _Bool scc_rbtree_impl_remove(void *rbtree, size_t elemsize);
 
-/**
- * Remove element from the ``rbtree``
- *
- * The \a value parameter must not necessarily be the same type as the one with which the
- * ``rbtree`` was instantiated. If it is not, the value is implicitly converted to the type
- * stored in the tree.
- *
- * \param rbtree Handle identifying the ``rbtree``
- * \param value The element to be removed
- *
- * \return ``true`` if the element was removed, otherwise ``false``
- */
 #define scc_rbtree_remove(rbtree, value)                                                    \
     scc_rbtree_impl_remove((*(rbtree) = (value), (rbtree)), sizeof(*(rbtree)))
 
@@ -391,50 +234,9 @@ inline void const *scc_rbtree_impl_iterstop(void const *rbtree) {
 
 void *scc_rbtree_impl_clone(void const *rbtree, size_t elemsize);
 
-/**
- * Clone the given ``rbtree`` instance.
- *
- * The new instance is allocated on the heap and guaranteed to contain identical key-value pairs as the original.
- *
- * \param rbtree Handle identifying the ``rbtree`` to clone
- *
- * \return Handle to a dynamically allocated copy of the original ``rbtree``, or ``NULL`` on failure
- */
 #define scc_rbtree_clone(rbtree)                                                            \
     scc_rbtree_impl_clone(rbtree, sizeof(*(rbtree)))
 
-/**
- * \verbatim embed:rst:leading-asterisk
- *  .. _scc_rbtree_foreach:
- * \endverbatim
- *
- * Iterate over the nodes in the ``rbtree``
- *
- * Nodes are traversed in the order defined by the
- * @verbatim embed:rst:inline :ref:comparator <scc_rbcompare>` @endverbatim
- * used.
- *
- * The macro expands to a scope executed with the \a iter - an instance whose type is
- * created using @verbatim embed:rst:inline :ref:`scc_rbtree_iter <scc_rbtree_iter>` @endverbatim -
- * referring to each element in the tree.
- *
- * The element is accessed by dereferencing the \a iter parameter.
- *
- * \verbatim embed:rst:leading-asterisk
- *
- * .. literalinclude:: /../examples/rbtree/tree_iteration.c
- *      :caption: Example of iterating over elements in an ``rbtree``
- *      :start-after: int main
- *      :end-before: }
- *      :language: c
- *
- * \endverbatim
- *
- * \param iter An instance of a type generated using
- *              @verbatim embed:rst:inline :ref:`scc_rbtree_iter <scc_rbtree_iter>` @endverbatim.
- *              Used as iteration variable
- * \param rbmap Handle identifying the ``rbtree``
- */
 #define scc_rbtree_foreach(iter, rbtree)                                                    \
     for (void const *scc_pp_cat_expand(scc_rbtree_end_,__LINE__) =                           \
             (iter = scc_rbtree_impl_leftmost_value(rbtree),                                 \
@@ -442,15 +244,6 @@ void *scc_rbtree_impl_clone(void const *rbtree, size_t elemsize);
         iter != scc_pp_cat_expand(scc_rbtree_end_,__LINE__);                                \
         iter = scc_rbtree_impl_successor(iter))
 
-/**
- * Like @verbatim embed:rst:inline :ref:`scc_rbtree_foreach <scc_rbtree_foreach>` @endverbatim
- * except that the pairs are visited in reversed order.
- *
- * \param iter An instance of a type generated using
- *              @verbatim embed:rst:inline :ref:`scc_rbtree_iter <scc_rbtree_iter>` @endverbatim.
- *              Used as iteration variable
- * \param rbtree Handle identifying the ``rbtree``
- */
 #define scc_rbtree_foreach_reversed(iter, rbtree)                                           \
     for (void const *scc_pp_cat_expand(scc_rbtree_end_,__LINE__) =                           \
             (iter = scc_rbtree_impl_rightmost_value(rbtree),                                \
