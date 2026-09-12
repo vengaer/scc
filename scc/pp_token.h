e3c221ba96a7362537d1bf33cdec2b680204aa25
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:41:14 +0200

    pp_token: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- scc/pp_token.h
+++ scc/pp_token.h
@@ -7,11 +7,11 @@
 #ifndef SCC_PP_TOKEN_H
 #define SCC_PP_TOKEN_H
 
-#define scc_pp_str(x) #x
+#define scc_pp_str(x)        #x
 #define scc_pp_str_expand(x) scc_pp_str(x)
 
-#define scc_pp_cat(x,y) x ## y
-#define scc_pp_cat_expand(x,y) scc_pp_cat(x,y)
+#define scc_pp_cat(x, y)        x##y
+#define scc_pp_cat_expand(x, y) scc_pp_cat(x, y)
 
 #define scc_pp_expand(...) __VA_ARGS__
 
