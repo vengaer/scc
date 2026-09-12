6556de07f47c659d52bb5f3168ed0d57be07a35e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:39:51 +0200

    mem: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- scc/mem.h
+++ scc/mem.h
@@ -10,20 +10,20 @@
 #include <assert.h>
 #include <stddef.h>
 
-#define scc_container_qual(addr, type, member, qual)    \
+#define scc_container_qual(addr, type, member, qual)                                               \
     ((type qual *)((unsigned char qual *)(addr) - offsetof(type, member)))
 
-#define scc_container(addr, type, member)               \
-    scc_container_qual(addr, type, member,)
+#define scc_container(addr, type, member) scc_container_qual(addr, type, member, )
 
-#define scc_alignof(type)                               \
-    sizeof(struct { unsigned char b; type e[]; })
+#define scc_alignof(type)                                                                          \
+    sizeof(struct {                                                                                \
+        unsigned char b;                                                                           \
+        type e[];                                                                                  \
+    })
 
-#define scc_arrsize(addr)                               \
-    (sizeof(addr) / sizeof((addr)[0]))
+#define scc_arrsize(addr) (sizeof(addr) / sizeof((addr)[0]))
 
-#define scc_align(addr, bound)                          \
-    (((addr) + (bound) - 1u) & ~((bound) - 1u))
+#define scc_align(addr, bound) (((addr) + (bound) - 1u) & ~((bound) - 1u))
 
 #ifdef SCC_INTERCEPT_NULLSIZE_COPIES
 
@@ -31,16 +31,14 @@
 #error INTERCEPT_NULLSIZE_COPIES has no effect with NDEBUG defined
 #endif
 
-#define scc_memmove(dst, src, n)                        \
-    (assert((n) > 0u), memmove((dst), (src), (n)))
+#define scc_memmove(dst, src, n) (assert((n) > 0u), memmove((dst), (src), (n)))
 
-#define scc_memcpy(dst, src, n)                         \
-    (assert((n) > 0u), memcpy((dst), (src), (n)))
+#define scc_memcpy(dst, src, n) (assert((n) > 0u), memcpy((dst), (src), (n)))
 
 #else
 
 #define scc_memmove(...) memmove(__VA_ARGS__)
-#define scc_memcpy(...) memcpy(__VA_ARGS__)
+#define scc_memcpy(...)  memcpy(__VA_ARGS__)
 
 #endif
 
