12a6f371653b02825bbc6abcc48e3b39fb36b93a
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:40:39 +0200

    stack: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- scc/stack.h
+++ scc/stack.h
@@ -16,26 +16,28 @@
 
 #define scc_stack(type) scc_pp_expand(SCC_STACK_CONTAINER)(type)
 
-#define scc_stack_new(type) scc_pp_cat_expand(SCC_STACK_CONTAINER,_new)(type)
+#define scc_stack_new(type) scc_pp_cat_expand(SCC_STACK_CONTAINER, _new)(type)
 
-#define scc_stack_new_dyn(type) scc_pp_cat_expand(SCC_STACK_CONTAINER,_new)(type)
+#define scc_stack_new_dyn(type) scc_pp_cat_expand(SCC_STACK_CONTAINER, _new)(type)
 
-#define scc_stack_free(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_free)(stack)
+#define scc_stack_free(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER, _free)(stack)
 
-#define scc_stack_push(stackaddr, ...) scc_pp_cat_expand(SCC_STACK_CONTAINER,_push)(stackaddr, (__VA_ARGS__))
+#define scc_stack_push(stackaddr, ...)                                                             \
+    scc_pp_cat_expand(SCC_STACK_CONTAINER, _push)(stackaddr, (__VA_ARGS__))
 
-#define scc_stack_pop(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_pop)(stack)
+#define scc_stack_pop(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER, _pop)(stack)
 
-#define scc_stack_top(stack) stack[scc_pp_cat_expand(SCC_STACK_CONTAINER,_size)(stack) - 1]
+#define scc_stack_top(stack) stack[scc_pp_cat_expand(SCC_STACK_CONTAINER, _size)(stack) - 1]
 
-#define scc_stack_size(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_size)(stack)
+#define scc_stack_size(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER, _size)(stack)
 
 #define scc_stack_empty(stack) (!scc_stack_size(stack))
 
-#define scc_stack_capacity(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_capacity)(stack)
+#define scc_stack_capacity(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER, _capacity)(stack)
 
-#define scc_stack_clear(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER,_clear)(stack)
+#define scc_stack_clear(stack) scc_pp_cat_expand(SCC_STACK_CONTAINER, _clear)(stack)
 
-#define scc_stack_reserve(stack, capacity) scc_pp_cat_expand(SCC_STACK_CONTAINER,_reserve)(stack, capacity)
+#define scc_stack_reserve(stack, capacity)                                                         \
+    scc_pp_cat_expand(SCC_STACK_CONTAINER, _reserve)(stack, capacity)
 
 #endif /* SCC_STACK_H */
