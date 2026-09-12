ae42579df87b70fb824d15c909bc7d67015cab68
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:37:39 +0200

    murmur64: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- lib/murmur64.c
+++ lib/murmur64.c
@@ -18,11 +18,13 @@
 
 #ifdef SCC_HAVE_UINT64_T
 
-static inline uint_fast64_t scc_rol64(uint64_t value, uint_fast8_t by) {
+static inline uint_fast64_t scc_rol64(uint64_t value, uint_fast8_t by)
+{
     return (value << by) | (value >> (64u - by));
 }
 
-static inline uint_fast64_t scc_fmix64(uint64_t h) {
+static inline uint_fast64_t scc_fmix64(uint64_t h)
+{
     h ^= h >> 33u;
     h *= UINT64_C(0xff51afd7ed558ccd);
     h ^= h >> 33u;
@@ -31,8 +33,9 @@ static inline uint_fast64_t scc_fmix64(uint64_t h) {
     return h;
 }
 
-static inline void scc_murmur64_128_main_calc(uint64_t hs[static restrict 2u],
-                uint64_t k1, uint64_t k2) {
+static inline void scc_murmur64_128_main_calc(uint64_t hs[static restrict 2u], uint64_t k1,
+                                              uint64_t k2)
+{
 #define h1 hs[0u]
 #define h2 hs[1u]
     k1 *= SCC_MURMUR64_C1;
@@ -59,7 +62,8 @@ static inline void scc_murmur64_128_main_calc(uint64_t hs[static restrict 2u],
 }
 
 static inline void scc_murmur64_128_unaligned_main(uint64_t hs[static restrict 2u],
-                void const *data, size_t size) {
+                                                   void const *data, size_t size)
+{
     unsigned long res = (unsigned long)data & (scc_alignof(uint64_t) - 1u);
     uint64_t tmp = UINT64_C(0);
     memcpy((unsigned char *)&tmp + sizeof(uint64_t) - res, data, res);
@@ -81,7 +85,8 @@ static inline void scc_murmur64_128_unaligned_main(uint64_t hs[static restrict 2
 }
 
 static inline void scc_murmur64_128_aligned_main(uint64_t hs[static restrict 2u],
-                    void const *restrict data, size_t size) {
+                                                 void const *restrict data, size_t size)
+{
     uint64_t const *p64 = data;
     for (unsigned i = 0u; i < size >> 4u; ++i) {
         uint64_t k1 = p64[i << 1u];
@@ -90,8 +95,9 @@ static inline void scc_murmur64_128_aligned_main(uint64_t hs[static restrict 2u]
     }
 }
 
-static inline void scc_murmur64_128_residual(uint64_t hs[static restrict 1u],
-                void const *data, size_t size) {
+static inline void scc_murmur64_128_residual(uint64_t hs[static restrict 1u], void const *data,
+                                             size_t size)
+{
     unsigned char const *p = data;
     p += size & ~15u;
 
@@ -99,12 +105,18 @@ static inline void scc_murmur64_128_residual(uint64_t hs[static restrict 1u],
     uint64_t k2 = UINT64_C(0);
 
     switch (size & 15u) {
-    case 15u: k2 ^= (uint64_t)p[14u] << 48u;
-    case 14u: k2 ^= (uint64_t)p[13u] << 40u;
-    case 13u: k2 ^= (uint64_t)p[12u] << 32u;
-    case 12u: k2 ^= (uint64_t)p[11u] << 24u;
-    case 11u: k2 ^= (uint64_t)p[10u] << 16u;
-    case 10u: k2 ^= (uint64_t)p[9u] << 8u;
+    case 15u:
+        k2 ^= (uint64_t)p[14u] << 48u;
+    case 14u:
+        k2 ^= (uint64_t)p[13u] << 40u;
+    case 13u:
+        k2 ^= (uint64_t)p[12u] << 32u;
+    case 12u:
+        k2 ^= (uint64_t)p[11u] << 24u;
+    case 11u:
+        k2 ^= (uint64_t)p[10u] << 16u;
+    case 10u:
+        k2 ^= (uint64_t)p[9u] << 8u;
     case 9u:
         k2 ^= (uint64_t)p[8u];
         k2 *= SCC_MURMUR64_C2;
@@ -112,13 +124,20 @@ static inline void scc_murmur64_128_residual(uint64_t hs[static restrict 1u],
         k2 *= SCC_MURMUR64_C1;
         hs[1u] ^= k2;
         /* fallthrough */
-    case 8u: k1 ^= (uint64_t)p[7u] << 56u;
-    case 7u: k1 ^= (uint64_t)p[6u] << 48u;
-    case 6u: k1 ^= (uint64_t)p[5u] << 40u;
-    case 5u: k1 ^= (uint64_t)p[4u] << 32u;
-    case 4u: k1 ^= (uint64_t)p[3u] << 24u;
-    case 3u: k1 ^= (uint64_t)p[2u] << 16u;
-    case 2u: k1 ^= (uint64_t)p[1u] << 8u;
+    case 8u:
+        k1 ^= (uint64_t)p[7u] << 56u;
+    case 7u:
+        k1 ^= (uint64_t)p[6u] << 48u;
+    case 6u:
+        k1 ^= (uint64_t)p[5u] << 40u;
+    case 5u:
+        k1 ^= (uint64_t)p[4u] << 32u;
+    case 4u:
+        k1 ^= (uint64_t)p[3u] << 24u;
+    case 3u:
+        k1 ^= (uint64_t)p[2u] << 16u;
+    case 2u:
+        k1 ^= (uint64_t)p[1u] << 8u;
     case 1u:
         k1 ^= (uint64_t)p[0u];
         break;
@@ -130,9 +149,10 @@ static inline void scc_murmur64_128_residual(uint64_t hs[static restrict 1u],
     hs[0u] ^= k1;
 }
 
-void scc_murmur64_128(struct scc_digest128 *digest, void const *data,
-        size_t size, uint_fast32_t seed) {
-    uint64_t hs[] = { seed, seed };
+void scc_murmur64_128(struct scc_digest128 *digest, void const *data, size_t size,
+                      uint_fast32_t seed)
+{
+    uint64_t hs[] = {seed, seed};
 
     if ((unsigned long)data & (scc_alignof(uint64_t) - 1u))
         scc_murmur64_128_unaligned_main(hs, data, size);
