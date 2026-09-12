d2f65319f297719c2fa9b59af7c4a71a2bbbf6f5
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:33:12 +0200

    arch: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- scc/arch.h
+++ scc/arch.h
@@ -12,7 +12,6 @@
 
 #include <stddef.h>
 
-
 #ifdef SCC_HWVEC_SIZE
 #define SCC_VECSIZE SCC_HWVEC_SIZE
 #else
@@ -20,86 +19,54 @@
 #endif
 
 #ifdef SCC_SIMD_ISA
-#define scc_arch_select(func)       \
-    scc_pp_cat_expand(              \
-        scc_pp_cat_expand(          \
-            scc_pp_cat_expand(      \
-                scc_pp_cat_expand(  \
-                    func,_          \
-                ),SCC_SIMD_ISA      \
-            ),_                     \
-        ),trampoline                \
-    )
+#define scc_arch_select(func)                                                                      \
+    scc_pp_cat_expand(                                                                             \
+        scc_pp_cat_expand(scc_pp_cat_expand(scc_pp_cat_expand(func, _), SCC_SIMD_ISA), _),         \
+        trampoline)
 
 #else
-#define scc_arch_select(func)   \
-    scc_pp_cat_expand(scc_pp_cat_expand(func,_),swar)
+#define scc_arch_select(func) scc_pp_cat_expand(scc_pp_cat_expand(func, _), swar)
 #endif
 
 struct scc_hashmap_base;
 struct scc_hashtab_base;
 
 extern unsigned long long scc_arch_select(scc_hashmap_impl_probe_insert)(
-    struct scc_hashmap_base const *base,
-    void const *map,
-    size_t keysize,
-    unsigned long long hash
-);
+    struct scc_hashmap_base const *base, void const *map, size_t keysize, unsigned long long hash);
 
-extern long long scc_arch_select(scc_hashmap_impl_probe_find)(
-    struct scc_hashmap_base const *base,
-    void const *map,
-    size_t keysize,
-    unsigned long long hash
-);
+extern long long scc_arch_select(scc_hashmap_impl_probe_find)(struct scc_hashmap_base const *base,
+                                                              void const *map, size_t keysize,
+                                                              unsigned long long hash);
 
 extern unsigned long long scc_arch_select(scc_hashtab_impl_probe_insert)(
-    struct scc_hashtab_base const *base,
-    void const *tab,
-    size_t elemsize,
-    unsigned long long hash
-);
+    struct scc_hashtab_base const *base, void const *tab, size_t elemsize, unsigned long long hash);
 
-extern long long scc_arch_select(scc_hashtab_impl_probe_find)(
-    struct scc_hashtab_base const *base,
-    void const *tab,
-    size_t elemsize,
-    unsigned long long hash
-);
+extern long long scc_arch_select(scc_hashtab_impl_probe_find)(struct scc_hashtab_base const *base,
+                                                              void const *tab, size_t elemsize,
+                                                              unsigned long long hash);
 
-inline unsigned long long scc_hashmap_impl_probe_insert(
-    struct scc_hashmap_base const *base,
-    void const *map,
-    size_t keysize,
-    unsigned long long hash
-) {
+inline unsigned long long scc_hashmap_impl_probe_insert(struct scc_hashmap_base const *base,
+                                                        void const *map, size_t keysize,
+                                                        unsigned long long hash)
+{
     return scc_arch_select(scc_hashmap_impl_probe_insert)(base, map, keysize, hash);
 }
 
-inline long long scc_hashmap_impl_probe_find(
-    struct scc_hashmap_base const *base,
-    void const *map,
-    size_t keysize,
-    unsigned long long hash
-) {
+inline long long scc_hashmap_impl_probe_find(struct scc_hashmap_base const *base, void const *map,
+                                             size_t keysize, unsigned long long hash)
+{
     return scc_arch_select(scc_hashmap_impl_probe_find)(base, map, keysize, hash);
 }
 
-inline long long scc_hashtab_impl_probe_insert(
-    struct scc_hashtab_base const *base,
-    void const *tab,
-    size_t elemsize,
-    unsigned long long hash
-) {
+inline long long scc_hashtab_impl_probe_insert(struct scc_hashtab_base const *base, void const *tab,
+                                               size_t elemsize, unsigned long long hash)
+{
     return scc_arch_select(scc_hashtab_impl_probe_insert)(base, tab, elemsize, hash);
 }
 
-inline long long scc_hashtab_impl_probe_find(
-    struct scc_hashtab_base const *base,
-    void const *tab,
-    size_t elemsize,
-    unsigned long long hash
-) {
+inline long long scc_hashtab_impl_probe_find(struct scc_hashtab_base const *base, void const *tab,
+                                             size_t elemsize, unsigned long long hash)
+{
     return scc_arch_select(scc_hashtab_impl_probe_find)(base, tab, elemsize, hash);
 }
 
