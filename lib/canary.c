7c10aef070851ed877c871fb9be19c9765571505
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 16:37:49 +0200

    canary: Apply configured formatting
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

--- lib/canary.c
+++ lib/canary.c
@@ -11,11 +11,13 @@
 #define CANARY_INITIAL 0xd4u
 
 #ifdef SCC_CANARY_ENABLED
-static inline unsigned char scc_canary_next_byte(unsigned char n) {
+static inline unsigned char scc_canary_next_byte(unsigned char n)
+{
     return (n << 1) ^ n;
 }
 
-void scc_canary_init(unsigned char *canary, size_t size) {
+void scc_canary_init(unsigned char *canary, size_t size)
+{
     unsigned char c = CANARY_INITIAL;
     for (unsigned i = 0u; i < size; ++i) {
         canary[i] = c;
@@ -23,7 +25,8 @@ void scc_canary_init(unsigned char *canary, size_t size) {
     }
 }
 
-_Bool scc_canary_intact(unsigned char const *canary, size_t size) {
+_Bool scc_canary_intact(unsigned char const *canary, size_t size)
+{
     unsigned char c = CANARY_INITIAL;
     bool intact = true;
     for (unsigned i = 0u; i < size && intact; ++i) {
--- scc/canary.h
+++ scc/canary.h
@@ -12,16 +12,15 @@
 #include <stddef.h>
 
 #ifdef SCC_CANARY_ENABLED
-#define SCC_CANARY_INJECT(size) unsigned char scc_pp_cat_expand(canary,__LINE__)[size];
+#define SCC_CANARY_INJECT(size) unsigned char scc_pp_cat_expand(canary, __LINE__)[size];
 
 void scc_canary_init(unsigned char *canary, size_t size);
 
 _Bool scc_canary_intact(unsigned char const *canary, size_t size);
 #else
 #define SCC_CANARY_INJECT(...)
-#define scc_canary_init(...) ((void)0)
+#define scc_canary_init(...)   ((void)0)
 #define scc_canary_intact(...) (_Bool)1
 #endif
-
 
 #endif /* SCC_CANARY_H */
